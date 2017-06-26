c9842244a224c1271662b8d707f0ad3cbde22feb
kanishk1010 <kanishk.dugar@outlook.com>
2017-06-26 18:16:18 -0400

    added script to retrieve ssh keys

---
+++ user_keys.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+# Obtain the public ssh keys of each of the public members of ole-vi organization
+
+import json
+import urllib2
+
+# Retrieve a list of public members using gitHub API
+api = 'https://api.github.com/orgs/ole-vi/members'
+json_data = json.load(urllib2.urlopen(api))
+members = []
+for member in json_data:
+    members.append(member['login'].encode('utf-8'))
+
+# Write the keys to /home/pi/.ssh/authorized_keys    
+f = open('/home/pi/.ssh/authorized_keys','w')
+for member in members:
+    f.write(urllib2.urlopen('https://github.com/' + member + '.keys').read())
+f.close()
