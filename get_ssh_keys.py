edc8f367d1d1146f9d1e3cb4987f806adfbb167f
kanishk1010 <kanishk.dugar@outlook.com>
2017-07-06 01:53:40 -0400

    updated get_ssh_keys.py

--- get_ssh_keys.py
+++ get_ssh_keys.py
@@ -6,7 +6,7 @@ import json
 import urllib2
 
 # Retrieve a list of public members using gitHub API
-api = 'https://api.github.com/orgs/ole-vi/members'
+api = 'https://api.github.com/orgs/open-learning-exchange/members'
 json_data = json.load(urllib2.urlopen(api))
 members = []
 for member in json_data:
