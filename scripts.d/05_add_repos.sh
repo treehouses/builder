2ebb994cc0cadb71d1f06960509772d650a31db8
Dafydd Harries <daf@rhydd.org>
2017-07-13 17:38:22 -0400

    05_add_repos.sh: only add sources list if it doesn't already exist

--- scripts.d/05_add_repos.sh
+++ scripts.d/05_add_repos.sh
@@ -2,18 +2,41 @@
 
 source lib.sh
 
+is_installed() {
+    pkg="$1"
+    dpkg-query -s "$pkg" 2>/dev/null | grep -qx 'Status: install ok installed'
+}
+
+install_stuff() {
+    local need_install
+    pkgs="debian-archive-keyring apt-transport-https"
+
+    for pkg in $pkgs; do
+        if ! is_installed $pkg; then
+            need_install="$need_install $pkg"
+            echo "need to install $pkg"
+        fi
+    done
+
+    if [ -n "$need_install" ]; then
+        echo "updating package sources"
+        _apt update || die "Could not update package sources"
+        _apt install $pkgs
+    fi
+}
+
 # List of extra APT repositories
 ADD_REPOS=(
     "deb https://packagecloud.io/Hypriot/Schatzkiste/raspbian/ jessie main"
     "deb-src https://packagecloud.io/Hypriot/Schatzkiste/raspbian/ jessie main"
 )
 
-if [[ "${ADD_REPOS:-}" ]] ; then
-    _apt update || die "Could not update package sources"
-    # Is this necessary?
-    _apt install debian-archive-keyring apt-transport-https || die "Could not install debian-archive-keyring apt-transport-https"
+LIST=mnt/img_root/etc/apt/sources.list.d/treehouse-builder.list
+
+if [[ "${ADD_REPOS:-}" && ! -f "$LIST" ]] ; then
+    install_stuff
     for repo in "${ADD_REPOS[@]}" ; do
         echo "$repo"
-    done > mnt/img_root/etc/apt/sources.list.d/treehouse-builder.list || die "Could not add repos ${ADD_REPOS[@]}"
+    done > $LIST || die "Could not add repos ${ADD_REPOS[@]}"
     _apt update || die "Could not update package sources"
 fi
---
+++ scripts.d/30_g_ether.sh
@@ -0,0 +1,32 @@
+
+# Make the pi into a USB ethernet gadget.
+
+CONFIG=/boot/config.txt
+CMDLINE=/boot/cmdline.txt
+IFACES=/etc/network/interfaces
+
+# Set up firmware configuration
+
+if !grep -q "^dtoverlay=" $CONFIG; then
+    #sed -i -e '${' -e 'a\\' -e 'a# Enable USB gadget mode' \
+    #    -e 'adtoverlay=dwc2' -e '}' /boot/config.txt
+    echo >> $CONFIG
+    echo "# Enable USB gadget mode" >> $CONFIG
+    echo "dtoverlay=dwc2" >> $CONFIG
+fi
+
+# Load necessary modules
+
+if !grep -q dwc2 $CMDLINE; then
+    sed -i -e 's/$/ modules-load=dwc2,g_ether/' $CMDLINE
+fi
+
+# Configure the network interface
+
+if !grep -q usb0 $IFACES; then
+    echo >> $IFACES
+    echo "auto usb0" >> $IFACES
+    echo "allow-hotplug usb0" >> $IFACES
+    echo "iface usb0 inet ipv4ll" >> $IFACES
+fi
+
