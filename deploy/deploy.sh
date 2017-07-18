123dea6d5723b52531d6e9f55aae9e89ea4f5722
Dafydd Harries <daf@rhydd.org>
2017-07-18 08:45:51 -0400

    deployment: add known_hosts file to prevent ssh prompt

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 image=$(ls images/*.img | head -1) # XXX
 image_gz=${image}.gz
+ssh=ssh -i deploy/id_deploy -o "UserKnownHostsFile deploy/known_hosts"
 gzip -9 $image
-rsync -P -e 'ssh -i deploy/id_deploy' $image_gz deploy@dev.ole.org:/data/deploy
+rsync -P -e "$ssh" $image_gz deploy@dev.ole.org:/data/deploy
