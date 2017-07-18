88705eed3117668c86b67523ffdc50f7dbf40a11
Dafydd Harries <daf@rhydd.org>
2017-07-18 11:21:12 -0400

    deploy: set -x to help debugging

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -2,6 +2,7 @@
 image=$(ls images/*.img | head -1) # XXX
 image_gz=${image}.gz
 ssh='ssh -i deploy/id_deploy -o "UserKnownHostsFile deploy/known_hosts"'
+set -x
 if [ \( ! -e $image_gz \) -o \( $image_gz -ot $image \) ]; then
      gzip -9k $image
 fi
