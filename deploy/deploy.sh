a85557d3c7a05c31bda5937ff19fad9c6f8cfb1a
Dafydd Harries <daf@rhydd.org>
2017-07-18 11:23:46 -0400

    deploy: make known_hosts a "global" rather than "user" file
    This means that ssh doesn't modify the file, which we don't want.

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -2,7 +2,7 @@
 image=$(ls images/*.img | head -1) # XXX
 test -n "$image" || exit 1
 image_gz=${image}.gz
-ssh='ssh -i deploy/id_deploy -o "UserKnownHostsFile deploy/known_hosts"'
+ssh='ssh -i deploy/id_deploy -o "GlobalKnownHostsFile deploy/known_hosts"'
 set -x
 if [ \( ! -e $image_gz \) -o \( $image_gz -ot $image \) ]; then
      gzip -9k $image
