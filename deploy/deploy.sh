d462fd2430a5bb0f350bfc73c2f5aade2c488efc
Dafydd Harries <daf@rhydd.org>
2017-07-18 11:20:41 -0400

    deploy: only gzip if necessary; keep uncompressed image

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -2,5 +2,7 @@
 image=$(ls images/*.img | head -1) # XXX
 image_gz=${image}.gz
 ssh='ssh -i deploy/id_deploy -o "UserKnownHostsFile deploy/known_hosts"'
-gzip -9 $image
+if [ \( ! -e $image_gz \) -o \( $image_gz -ot $image \) ]; then
+     gzip -9k $image
+fi
 rsync -P -e "$ssh" $image_gz deploy@dev.ole.org:/data/deploy
