47b6da0011c72b0bc24b8c67daa0caf1f601b632
Dafydd Harries <daf@rhydd.org>
2017-07-19 17:03:43 -0400

    deploy.sh: set -e

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -3,6 +3,7 @@ image=$(ls images/*.img | head -1) # XXX
 test -n "$image" || exit 1
 image_gz=${image}.gz
 ssh='ssh -i deploy/id_deploy -o "GlobalKnownHostsFile deploy/known_hosts"'
+set -e
 set -x
 chmod 600 deploy/id_deploy
 if [ \( ! -e $image_gz \) -o \( $image_gz -ot $image \) ]; then
