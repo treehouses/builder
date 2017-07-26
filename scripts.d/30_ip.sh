35906acb0288762931aa83140992850e700a1be8
Dafydd Harries <daf@rhydd.org>
2017-07-26 14:41:06 -0400

    work around the fact that npm doesn't work under Travis qemu
    Run npm on the host system, then copy the installed files to the target system.

--- scripts.d/30_ip.sh
+++ scripts.d/30_ip.sh
@@ -2,10 +2,13 @@
 
 source lib.sh
 
-echo "Installing node"
-
-_apt install nodejs-legacy npm
-
 echo "Installing ip script"
 
-_chroot npm install -g pirate-sh
+mkdir node_root
+npm config set prefix $PWD/node_root
+npm install -g pirate-sh
+(cd node_root; tar c .) | (cd mnt/img_root/usr/local; tar x)
+npm config delete prefix
+rm -rf node_root
+
+
