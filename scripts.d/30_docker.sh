990fa1f81a9b5bede53ab830f3e1f35cdd32b08b
Dafydd Harries <daf@rhydd.org>
2017-08-04 11:06:16 -0400

    compress saved docker image

--- scripts.d/30_docker.sh
+++ scripts.d/30_docker.sh
@@ -2,4 +2,4 @@
 
 image=dogi/rpi-couchdb
 sudo docker pull $image
-sudo docker save -o mnt/img_root/root/rpi-couchdb.docker $image
+sudo docker save $image | gzip -c9 > mnt/img_root/root/rpi-couchdb.tar.gz
