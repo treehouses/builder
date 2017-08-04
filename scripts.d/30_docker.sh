0cfa25ce22f5bab5ba53a88637ccb79aa3af1007
Dafydd Harries <daf@rhydd.org>
2017-08-04 11:40:55 -0400

    load Docker image on boot if present

--- scripts.d/30_docker.sh
+++ scripts.d/30_docker.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+source lib.sh
+
 image=dogi/rpi-couchdb
 sudo docker pull $image
 sudo docker save $image | gzip -c9 > mnt/img_root/root/rpi-couchdb.tar.gz
+_op _chroot systemctl enable docker-load
