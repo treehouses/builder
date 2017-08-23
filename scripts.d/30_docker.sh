d7763c1e76f2795172934b3af314c110c234829b
Dafydd Harries <daf@rhydd.org>
2017-08-23 18:28:04 -0400

    add pi user to the docker group

--- scripts.d/30_docker.sh
+++ scripts.d/30_docker.sh
@@ -6,3 +6,4 @@ image=dogi/rpi-couchdb
 sudo docker pull $image
 sudo docker save $image | gzip -c9 > mnt/img_root/root/rpi-couchdb.tar.gz
 _op _chroot systemctl enable docker-load
+_op _chroot adduser pi docker
