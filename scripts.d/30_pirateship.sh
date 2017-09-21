ee6fbf913149ab0efd6c98176616ee8518e32de2
dogi <dogi@users.noreply.github.com>
2017-09-20 21:33:57 -0400

    Update 30_pirateship.sh

--- scripts.d/30_pirateship.sh
+++ scripts.d/30_pirateship.sh
@@ -6,7 +6,7 @@ echo "Installing ip script"
 
 mkdir node_root
 npm config set prefix $PWD/node_root
-npm install -g @pirateship/cli@0.2.2
+npm install -g @pirateship/cli@0.2.3
 (cd node_root; tar c .) | (cd mnt/img_root/usr/local; tar x)
 npm config delete prefix
 rm -rf node_root
