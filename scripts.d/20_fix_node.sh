2838876d3691fd994fcc28891c94b5715075cae4
dogi <dogi@users.noreply.github.com>
2017-09-27 21:14:34 -0400

    Create 20_fix_node.sh

---
+++ scripts.d/20_fix_node.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+source lib.sh
+
+echo "Fix node"
+wget https://nodejs.org/dist/v8.5.0/node-v8.5.0-linux-armv6l.tar.gz
+tar xvzf node-v8.5.0-linux-armv6l.tar.gz node-v8.5.0-linux-armv6l/bin/node
+
+_op _chroot mv /usr/bin/node /usr/bin/node-armv7l
+_op _chroot (cd /usr/bin; ln -s node-armv7l node)
+(cd node-v8.5.0-linux-armv6l/bin; tar c .) | _op _chroot tar vx --owner=root --group=root
