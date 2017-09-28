986f121b917209fdf0b70a9e3949bfb1f1f373bc
dogi <dogi@users.noreply.github.com>
2017-09-27 20:47:48 -0400

    Update 15_pkg_upgrade.sh

--- scripts.d/15_pkg_upgrade.sh
+++ scripts.d/15_pkg_upgrade.sh
@@ -2,6 +2,9 @@
 
 source lib.sh
 
+echo "Holding nodejs Package"
+_op _chroot apt-mark hold nodejs
+
 echo "Installing Updates"
 _apt update || die "Could not update package sources"
 _apt dist-upgrade || die "Could not upgrade system"
