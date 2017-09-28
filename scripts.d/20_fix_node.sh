a3eefd5d51ad2d5c476aa9cd93c0083ae1856444
dogi <dogi@users.noreply.github.com>
2017-09-28 19:11:55 -0400

    one node for all rpis (fixes #88) (#89)
    * Update 15_pkg_install.sh
    * Update 20_fix_node.sh
    * Update do_autorun

---
+++ scripts.d/20_fix_node.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+source lib.sh
+
+echo "Fix node"
+# armv6l
+wget https://nodejs.org/dist/v8.5.0/node-v8.5.0-linux-armv6l.tar.gz
+tar xvzf node-v8.5.0-linux-armv6l.tar.gz node-v8.5.0-linux-armv6l/bin/node
+mv node-v8.5.0-linux-armv6l/bin/node mnt/img_root/usr/bin/node-armv6l
+_op _chroot chown root:root /usr/bin/node-armv6l
+rm -rf node-v8.5.0-linux-armv6l
+
+# armv7l
+_op _chroot mv /usr/bin/node /usr/bin/node-armv7l
+_op _chroot ln -sr /usr/bin/node-armv7l /usr/bin/node
