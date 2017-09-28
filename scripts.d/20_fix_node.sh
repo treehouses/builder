ab67dbee559a48bd9317c18cf7a8a4ef9bcfbedf
dogi <dogi@users.noreply.github.com>
2017-09-28 00:26:07 -0400

    Update 20_fix_node.sh

--- scripts.d/20_fix_node.sh
+++ scripts.d/20_fix_node.sh
@@ -5,8 +5,9 @@ source lib.sh
 echo "Fix node"
 # armv6l
 wget https://nodejs.org/dist/v8.5.0/node-v8.5.0-linux-armv6l.tar.gz
-tar xvzf node-v8.5.0-linux-armv6l.tar.gz node-v8.5.0-linux-armv6l/bin/node --group=root --owner=root
+tar xvzf node-v8.5.0-linux-armv6l.tar.gz node-v8.5.0-linux-armv6l/bin/node
 mv node-v8.5.0-linux-armv6l/bin/node mnt/img_root/usr/bin/node-armv6l
+_op _chroot chown root:root /usr/bin/node-armv6l
 rm -rf node-v8.5.0-linux-armv6l
 
 # armv7l
