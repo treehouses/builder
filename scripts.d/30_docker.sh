b4b875ea0a4861c15e126b6d38ecca6b6448a70e
Ignacio Rodríguez <ignacio@sugarlabs.org>
2017-10-03 19:34:46 -0300

    removed docker service on first boot

--- scripts.d/30_docker.sh
+++ scripts.d/30_docker.sh
@@ -4,6 +4,6 @@ source lib.sh
 
 image=dogi/rpi-couchdb
 sudo docker pull $image
-sudo docker save $image | gzip -c9 > mnt/img_root/root/rpi-couchdb.tar.gz
-_op _chroot systemctl enable docker-load
+mkdir -p mnt/img_root/root/dockerimages/
+sudo docker save $image | gzip -c9 > mnt/img_root/root/dockerimages/rpi-couchdb.tar.gz
 _op _chroot adduser pi docker
