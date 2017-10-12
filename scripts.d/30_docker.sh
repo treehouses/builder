4966759daa8df8f3fa2cc267b17e667497848cbb
Andrew Phan <andyph666@gmail.com>
2017-10-11 23:21:49 -0400

    Update 30_docker.sh

--- scripts.d/30_docker.sh
+++ scripts.d/30_docker.sh
@@ -2,8 +2,20 @@
 
 source lib.sh
 
-image=dogi/rpi-couchdb
-sudo docker pull $image
-mkdir -p mnt/img_root/root/dockerimages/
-sudo docker save $image | gzip -c9 > mnt/img_root/root/dockerimages/rpi-couchdb.tar.gz
+function _getfilename {
+  echo $@ | tr '/' '-'
+}
+
+function _docker {
+  sudo docker pull $@
+  mkdir -p mnt/img_root/root/dockerimages/
+  sudo docker save $@ | gzip -c9 > mnt/img_root/root/dockerimages/$(_getfilename $@).tar.gz
+}
+
+#image=dogi/rpi-couchdb
+#sudo docker pull $image
+#mkdir -p mnt/img_root/root/dockerimages/
+#sudo docker save $image | gzip -c9 > mnt/img_root/root/dockerimages/rpi-couchdb.tar.gz
+_docker dogi/rpi-couchdb
+_docker portainer/portainer
 _op _chroot adduser pi docker
