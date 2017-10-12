ea07549ffb00377b55c8049403166cd4904c3208
dogi <dogi@users.noreply.github.com>
2017-10-11 23:30:52 -0400

    Update 30_docker.sh

--- scripts.d/30_docker.sh
+++ scripts.d/30_docker.sh
@@ -6,4 +6,10 @@ image=dogi/rpi-couchdb
 sudo docker pull $image
 mkdir -p mnt/img_root/root/dockerimages/
 sudo docker save $image | gzip -c9 > mnt/img_root/root/dockerimages/rpi-couchdb.tar.gz
+
+image2=portainer/portainer
+sudo docker pull $image2
+mkdir -p mnt/img_root/root/dockerimages/
+sudo docker save $image2 | gzip -c9 > mnt/img_root/root/dockerimages/portainer.tar.gz
+
 _op _chroot adduser pi docker
