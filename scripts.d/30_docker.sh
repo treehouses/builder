57a7022350d1589b9014c0d890cfeb817c3c925f
dogi <dogi@users.noreply.github.com>
2017-10-18 18:48:47 -0400

    Update 30_docker.sh

--- scripts.d/30_docker.sh
+++ scripts.d/30_docker.sh
@@ -8,14 +8,10 @@ function _getfilename {
 
 function _docker {
   sudo docker pull $@
-  mkdir -p mnt/img_root/root/dockerimages/
   sudo docker save $@ | gzip -c9 > mnt/img_root/root/dockerimages/$(_getfilename $@).tar.gz
 }
 
-#image=dogi/rpi-couchdb
-#sudo docker pull $image
-#mkdir -p mnt/img_root/root/dockerimages/
-#sudo docker save $image | gzip -c9 > mnt/img_root/root/dockerimages/rpi-couchdb.tar.gz
+mkdir -p mnt/img_root/root/dockerimages/
 _docker dogi/rpi-couchdb
 _docker portainer/portainer
 _op _chroot adduser pi docker
