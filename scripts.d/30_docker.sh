743a321e44666cc9f7690c736b8f7b9ada47ae4d
Ignacio Rodríguez <ignacio@sugarlabs.org>
2017-12-08 13:54:05 -0300

    Save images to /root/images

--- scripts.d/30_docker.sh
+++ scripts.d/30_docker.sh
@@ -8,10 +8,10 @@ function _getfilename {
 
 function _docker {
   sudo docker pull $@
-  sudo docker save $@ | gzip -c9 > mnt/img_root/root/dockerimages/$(_getfilename $@).tar.gz
+  sudo docker save $@ | gzip -c9 > mnt/img_root/root/images/$(_getfilename $@).tar.gz
 }
 
-mkdir -p mnt/img_root/root/dockerimages/
+mkdir -p mnt/img_root/root/images/
 _docker dogi/rpi-couchdb
 _docker portainer/portainer
 _op _chroot adduser pi docker
