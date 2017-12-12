8469ccc0a117c7c03f08b19c2b30826d1402abcb
dogi <dogi@users.noreply.github.com>
2017-12-12 00:42:24 -0500

    Update 30_pirateship.sh

--- scripts.d/30_pirateship.sh
+++ scripts.d/30_pirateship.sh
@@ -6,7 +6,7 @@ echo "Installing ip script"
 
 mkdir node_root
 npm config set prefix $PWD/node_root
-npm install --unsafe-perm -g @pirateship/cli@0.3.1-nodocker
+npm install --unsafe-perm -g @pirateship/cli@0.3.2
 (cd node_root; tar c .) | (cd mnt/img_root/usr; tar x)
 npm config delete prefix
 rm -rf node_root
