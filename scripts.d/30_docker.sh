57ecca98b37bf7b1c8ba2e4eb29f2e96a511cabd
Ignacio Rodríguez <ignacio@sugarlabs.org>
2017-12-11 22:31:59 -0300

    Copy the local /var/lib/docker to the image

--- scripts.d/30_docker.sh
+++ scripts.d/30_docker.sh
@@ -8,12 +8,26 @@ function _getfilename {
 
 function _docker {
   sudo docker pull $@
-  sudo docker save $@ | gzip -c9 > mnt/img_root/root/dockerimages/$(_getfilename $@).tar.gz
 }
 
-mkdir -p mnt/img_root/root/dockerimages/
 _docker dogi/rpi-couchdb
 _docker portainer/portainer
 _docker arm32v7/postgres
 _docker treehouses/moodle:rpi-latest
 _op _chroot adduser pi docker
+
+sudo rm -rf mnt/var/lib/docker
+
+was_docker_started=0
+if [ "`systemctl is-active docker`" == "active" ]
+then
+  was_docker_started=1
+  sudo systemctl stop docker
+fi
+
+sudo rsync -Pax /var/lib/docker mnt/var/lib/
+
+if [ "$was_docker_started" == 1 ]
+then
+  sudo systemctl start docker
+fi
