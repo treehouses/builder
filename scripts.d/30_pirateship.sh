e37633720c05a600de861df325f8aa92ba788ec3
Ignacio Rodríguez <ignacio@sugarlabs.org>
2017-12-12 00:13:21 -0300

    replace npm version with the no-docker one

--- scripts.d/30_pirateship.sh
+++ scripts.d/30_pirateship.sh
@@ -6,7 +6,7 @@ echo "Installing ip script"
 
 mkdir node_root
 npm config set prefix $PWD/node_root
-npm install --unsafe-perm -g @pirateship/cli@0.3.1
+npm install --unsafe-perm -g @pirateship/cli@0.3.1-nodocker
 (cd node_root; tar c .) | (cd mnt/img_root/usr; tar x)
 npm config delete prefix
 rm -rf node_root
