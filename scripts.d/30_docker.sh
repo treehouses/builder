f581c1dccc98553ca695a811a44ef7c0b5848b06
dogi <dogi@users.noreply.github.com>
2017-12-13 21:51:21 -0500

    Update 30_docker.sh

--- scripts.d/30_docker.sh
+++ scripts.d/30_docker.sh
@@ -17,5 +17,7 @@ _op _chroot adduser pi docker
 
 sudo rm -rf mnt/img_root/var/lib/docker
 sudo service docker stop
+sudo du -h -s -x /var/lib/docker/overlay2/*
 sudo rsync -aqxP /var/lib/docker mnt/img_root/var/lib/
+sudo du -h -s -x mnt/img_root/var/lib/docker/overlay2/*
 sudo service docker start
