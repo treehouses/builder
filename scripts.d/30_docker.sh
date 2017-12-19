d37aad898e9b555e0d8963336d05c6ee583e47e9
dogi <dogi@users.noreply.github.com>
2017-12-19 01:08:59 -0500

    Update 30_docker.sh

--- scripts.d/30_docker.sh
+++ scripts.d/30_docker.sh
@@ -10,8 +10,8 @@ IMAGES=(
 )
 
 sudo service docker stop
-mv /var/lib/docker /var/lib/docker.temp
-ln -sr `pwd -P`/mnt/img_root/var/lib/docker /var/lib/docker
+sudo mv /var/lib/docker /var/lib/docker.temp
+sudo ln -sr `pwd -P`/mnt/img_root/var/lib/docker /var/lib/docker
 sudo service docker start
 
 for image in "${IMAGES[@]}" ; do
@@ -21,6 +21,6 @@ done
 _op _chroot adduser pi docker
 
 sudo service docker stop
-unlink /var/lib/docker
-mv /var/lib/docker.temp /var/lib/docker
+sudo unlink /var/lib/docker
+sudo mv /var/lib/docker.temp /var/lib/docker
 sudo service docker start
