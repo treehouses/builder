0d0adba8883099391b652d19d1180d23c5178aa5
Ignacio Rodríguez <ignacio@sugarlabs.org>
2017-12-29 19:38:11 -0300

    Allow devices to connect after pirateship bluetooth is off (Fixes #173)

--- scripts.d/30_pirateship.sh
+++ scripts.d/30_pirateship.sh
@@ -6,7 +6,7 @@ echo "Installing ip script"
 
 mkdir node_root
 npm config set prefix $PWD/node_root
-npm install --unsafe-perm -g @pirateship/cli@0.3.4
+npm install --unsafe-perm -g @pirateship/cli@0.3.4-fixBluetooth0
 (cd node_root; tar c .) | (cd mnt/img_root/usr; tar x)
 npm config delete prefix
 rm -rf node_root
