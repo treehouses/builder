31bb097663abc5a7397864f4bab2c11032653043
Ignacio Rodríguez <ignacio@sugarlabs.org>
2018-01-04 01:25:15 -0300

    fix

--- scripts.d/10_pkg_purge.sh
+++ scripts.d/10_pkg_purge.sh
@@ -11,13 +11,14 @@ PURGE_PACKAGES=(
     dphys-swapfile
 )
 
-PURGE_PACKAGES=( "$(
+# shellcheck disable=SC2207
+PURGE_PACKAGES=( $(
     for package in "${PURGE_PACKAGES[@]}" ; do
         if _chroot dpkg -s "$package" &>/dev/null ; then
             echo "$package"
         fi
     done
-)" )
+) )
 
 if [[ ${PURGE_PACKAGES:-} ]] ; then
     echo "Removing unwanted packages ${PURGE_PACKAGES[*]}"
