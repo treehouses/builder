cf5f1535b7559e5384a464fa4e882c44906541da
Ignacio Rodríguez <ignacio@sugarlabs.org>
2018-01-04 00:59:43 -0300

    Fix shellcheck errors on scripts.d/30_enable_uart.sh

--- scripts.d/30_enable_uart.sh
+++ scripts.d/30_enable_uart.sh
@@ -8,7 +8,9 @@ CONFIG=$ROOT/boot/config.txt
 # Set up firmware configuration
 
 if ! grep -q "^enable_uart=" $CONFIG; then
-    echo >> $CONFIG
-    echo "# Enable UART on Pi 3" >> $CONFIG
-    echo "enable_uart=1" >> $CONFIG
+    {
+        echo
+        echo "# Enable UART on Pi 3"
+        echo "enable_uart=1"
+     } >> $CONFIG
 fi
