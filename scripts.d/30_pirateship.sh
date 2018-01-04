54db5de5c30a5a388a72944438235a382084983a
Ignacio Rodríguez <ignacio@sugarlabs.org>
2018-01-04 00:16:00 -0300

    Fix shellcheck errors on scripts.d/30_pirateship.sh

--- scripts.d/30_pirateship.sh
+++ scripts.d/30_pirateship.sh
@@ -5,8 +5,8 @@ source lib.sh
 echo "Installing ip script"
 
 mkdir node_root
-npm config set prefix $PWD/node_root
+npm config set prefix "$PWD/node_root"
 npm install --unsafe-perm -g @pirateship/cli@0.3.5
-(cd node_root; tar c .) | (cd mnt/img_root/usr; tar x)
+(cd node_root || exit 1; tar c .) | (cd mnt/img_root/usr || exit 1; tar x)
 npm config delete prefix
 rm -rf node_root
