aa51b2970d2282ef8380b1f8d3998eefaa56b125
Ignacio Rodríguez <ignacio@sugarlabs.org>
2018-01-08 01:38:17 -0300

    update 05_add_keys.sh (#198)

--- scripts.d/05_add_keys.sh
+++ scripts.d/05_add_keys.sh
@@ -11,22 +11,22 @@ ADD_REPO_KEYS=(
 
 GPG="gpg --no-permission-warning --no-default-keyring --quiet --keyring "
 KEYRING=$PWD/apt_keys.gpg
-rm -f $KEYRING
-cat keys/*.key | $GPG $KEYRING --import
+rm -f "$KEYRING"
+cat keys/*.key | $GPG "$KEYRING" --import
 
 missing_keys=()
 for key in "${ADD_REPO_KEYS[@]}" ; do
-    if ! $GPG $KEYRING --list-keys $key >/dev/null 2>&1 ; then
-        missing_keys+=( $key )
+    if ! $GPG "$KEYRING" --list-keys "$key" >/dev/null 2>&1 ; then
+        missing_keys+=( "$key" )
     fi
 done
 
-if [[ ${#missing_keys[@]} > 0 ]]; then
-    die "missing GPG keys! try: $GPG $KEYRING --recv-keys ${missing_keys[@]}"
+if [[ ${#missing_keys[@]} -gt 0 ]]; then
+    die "missing GPG keys! try: $GPG $KEYRING --recv-keys ${missing_keys[*]}"
 fi
 
-$GPG $KEYRING --export ${ADD_REPO_KEYS[@]} | \
+$GPG "$KEYRING" --export "${ADD_REPO_KEYS[@]}" | \
     $GPG mnt/img_root/etc/apt/trusted.gpg --trustdb-name mnt/img_root/etc/apt/trustdb.gpg --import - || \
-    die "Could not import GPG keys ${ADD_REPO_KEYS[@]} for apt"
-rm -f $KEYRING
+    die "Could not import GPG keys ${ADD_REPO_KEYS[*]} for apt"
+rm -f "$KEYRING"
 
