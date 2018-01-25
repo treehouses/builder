681c89b1cc02900786b49b3421c5ae091a071a74
Ignacio Rodríguez <ignacio@sugarlabs.org>
2018-01-24 21:11:19 -0300

    auto usb ethernet when rpi zero's (fixes #6) (#223)

--- scripts.d/30_g_ether.sh
+++ scripts.d/30_g_ether.sh
@@ -1,44 +1,25 @@
 #!/bin/bash
 
-# Disabled until we can make this conditional somehow
-exit 0
-
-set -e
-
 # Make the pi into a USB ethernet gadget.
-
 ROOT=mnt/img_root
 CONFIG=$ROOT/boot/config.txt
 CMDLINE=$ROOT/boot/cmdline.txt
-IFACES=$ROOT/etc/network/interfaces
+USB0IFACE=$ROOT/etc/network/interfaces.d/usb0
 
 # Set up firmware configuration
+{
+    echo
+    echo "# Enable USB gadget mode"
+    echo "#dtoverlay=dwc2"
+} >> $CONFIG
 
-if ! grep -q "^dtoverlay=" $CONFIG; then
-    #sed -i -e '${' -e 'a\\' -e 'a# Enable USB gadget mode' \
-    #    -e 'adtoverlay=dwc2' -e '}' /boot/config.txt
-    {
-        echo
-        echo "# Enable USB gadget mode"
-        echo "dtoverlay=dwc2" 
-    } >> $CONFIG
-fi
-
-# Load necessary modules
-
-if ! grep -q dwc2 $CMDLINE; then
-    sed -i -e 's/$/ modules-load=dwc2,g_ether/' $CMDLINE
-fi
+sed -i -e 's/$/ #modules-load=dwc2,g_ether/' $CMDLINE
 
 # Configure the network interface
-
-if ! grep -q usb0 $IFACES; then
-    {
-        echo
-        echo "auto usb0"
-        echo "allow-hotplug usb0"
-        echo "iface usb0 inet ipv4ll"
-    } >> $IFACES
-
-fi
+{
+    echo
+    echo "auto usb0"
+    echo "allow-hotplug usb0"
+    echo "iface usb0 inet ipv4ll"
+} >> $USB0IFACE
 
