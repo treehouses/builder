276d552e95923fecf0d644102ce17ea11ba2b252
Ignacio Rodríguez <ignacio@sugarlabs.org>
2018-04-11 19:29:52 -0300

    remove debug for bluetooth class changing

--- scripts.d/30_bluetooth.sh
+++ scripts.d/30_bluetooth.sh
@@ -3,8 +3,6 @@
 source lib.sh
 
 curl "https://raw.githubusercontent.com/ole-vi/bluetooth-server/master/bluetooth-server.py" -o mnt/img_root/usr/local/bin/bluetooth-server.py
-echo "before sed"
-cat mnt/img_root/etc/bluetooth/main.conf
+
+echo "Switching bluetooth device class to 0x00010c - computer"
 sed -i -e 's/#Class = .*/Class = 0x00010c/g' mnt/img_root/etc/bluetooth/main.conf
-echo "after sed"
-cat mnt/img_root/etc/bluetooth/main.conf
