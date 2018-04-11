d7eb5ff2b784e725abd5386381319fed8a08e83c
Ignacio Rodríguez <ignacio@sugarlabs.org>
2018-04-11 19:10:43 -0300

    added the computer class to the bluetooth config

--- scripts.d/30_bluetooth.sh
+++ scripts.d/30_bluetooth.sh
@@ -3,3 +3,8 @@
 source lib.sh
 
 curl "https://raw.githubusercontent.com/ole-vi/bluetooth-server/master/bluetooth-server.py" -o mnt/img_root/usr/local/bin/bluetooth-server.py
+echo "before sed"
+cat mnt/img_root/etc/bluetooth/main.conf
+sed -i -e 's/#Class = .*/Class = 0x00010c/g' mnt/img_root/etc/bluetooth/main.conf
+echo "after sed"
+cat mnt/img_root/etc/bluetooth/main.conf
