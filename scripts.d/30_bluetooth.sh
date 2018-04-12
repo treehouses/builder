68797d5a89b17e39bae7572547b863ba470a9a43
Ignacio Rodríguez <ignacio@sugarlabs.org>
2018-04-11 21:29:08 -0300

    set bluetooth from headphone to computer (#255)

--- scripts.d/30_bluetooth.sh
+++ scripts.d/30_bluetooth.sh
@@ -3,3 +3,6 @@
 source lib.sh
 
 curl "https://raw.githubusercontent.com/ole-vi/bluetooth-server/master/bluetooth-server.py" -o mnt/img_root/usr/local/bin/bluetooth-server.py
+
+echo "Switching bluetooth device class to 0x00010c - computer"
+sed -i -e 's/#Class = .*/Class = 0x00010c/g' mnt/img_root/etc/bluetooth/main.conf
