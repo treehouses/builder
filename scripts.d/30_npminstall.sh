c8655ee51c5709e41ff344f69cdff37384276f63
dogi <dogi@users.noreply.github.com>
2018-05-02 23:28:54 -0400

    treehouses/cli@0.0.14 (#264)

--- scripts.d/30_npminstall.sh
+++ scripts.d/30_npminstall.sh
@@ -7,7 +7,7 @@ echo "Installing ip script"
 mkdir node_root
 npm config set prefix "$PWD/node_root"
 npm install --unsafe-perm -g @pirateship/cli@0.3.7
-npm install --unsafe-perm -g @treehouses/cli@0.0.13
+npm install --unsafe-perm -g @treehouses/cli@0.0.14
 (cd node_root || exit 1; tar c .) | (cd mnt/img_root/usr || exit 1; tar x)
 npm config delete prefix
 rm -rf node_root
