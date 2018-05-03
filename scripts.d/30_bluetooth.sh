6a7718561c9d55dbed33352b2c84cb532eb1dfb2
Ignacio Rodríguez <ignacio@sugarlabs.org>
2018-05-02 21:39:25 -0300

    better bluetooth server and treehouses/cli@0.0.13 (fixes #227) (fixes #239) (#262)

--- scripts.d/30_bluetooth.sh
+++ scripts.d/30_bluetooth.sh
@@ -2,7 +2,9 @@
 
 source lib.sh
 
-curl "https://raw.githubusercontent.com/ole-vi/bluetooth-server/master/bluetooth-server.py" -o mnt/img_root/usr/local/bin/bluetooth-server.py
+curl "https://raw.githubusercontent.com/treehouses/control/master/server.py" -o mnt/img_root/usr/local/bin/bluetooth-server.py
 
 echo "Switching bluetooth device class to 0x00010c - computer"
 sed -i -e 's/#Class = .*/Class = 0x00010c/g' mnt/img_root/etc/bluetooth/main.conf
+
+_pip3_install pybluez
