cbfe5acbbfd20ad67b6863871d394ed1d13d4487
dogi <stefan@unterhauser.name>
2018-05-11 22:29:17 -0400

    nojekyll

--- scripts.d/20_install_files.sh
+++ scripts.d/20_install_files.sh
@@ -2,4 +2,4 @@
 
 source lib.sh
 echo "Installing files"
-(cd install || die "ERROR: install folder doesn't exist, exiting"; tar c .) | _op _chroot tar vx --owner=root --group=root
+(cd .install || die "ERROR: .install folder doesn't exist, exiting"; tar c .) | _op _chroot tar vx --owner=root --group=root
