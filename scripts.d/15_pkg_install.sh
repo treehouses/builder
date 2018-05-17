bdcba82405299fa8ead6f4dc90b092395f354655
Ignacio Rodríguez <ignacio@sugarlabs.org>
2018-05-16 23:49:21 -0300

    treehouses bridge

--- scripts.d/15_pkg_install.sh
+++ scripts.d/15_pkg_install.sh
@@ -16,6 +16,7 @@ INSTALL_PACKAGES=(
     autossh
     python3-pip python3-dbus
     bluez minicom bluez-tools libbluetooth-dev # bluetooth hotspot
+    rng-tools # for ap bridge
 )
 
 if [[ ${INSTALL_PACKAGES:-} ]] ; then
