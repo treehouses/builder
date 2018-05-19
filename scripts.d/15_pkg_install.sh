5c483350bff7d14e81ed8fbc893e659521cabdf9
Ignacio Rodríguez <ignacio@sugarlabs.org>
2018-05-19 01:51:11 -0300

    usb otg network for RPIZ (#288)

--- scripts.d/15_pkg_install.sh
+++ scripts.d/15_pkg_install.sh
@@ -16,6 +16,7 @@ INSTALL_PACKAGES=(
     autossh
     python3-pip python3-dbus
     bluez minicom bluez-tools libbluetooth-dev # bluetooth hotspot
+    avahi-autoipd # for usb0
 )
 
 if [[ ${INSTALL_PACKAGES:-} ]] ; then
