7a42bcf4181905a28da5f93f0151be83ea527284
dogi <stefan@unterhauser.name>
2018-09-19 19:24:55 -0400

    next

--- scripts.d/40_planet.sh
+++ scripts.d/40_planet.sh
@@ -10,15 +10,15 @@ cd /root || exit 1
 wget https://raw.githubusercontent.com/open-learning-exchange/planet/master/docker/planet.yml
 #wget https://raw.githubusercontent.com/open-learning-exchange/planet/master/docker/volumes.yml
 {
-echo 'services:'
-echo '  couchdb:'
-echo '    volumes:'
-echo '      - "$(pwd -P)/mnt/img_root/srv/planet/data:/opt/couchdb/data"'
-echo '      - "$(pwd -P)/mnt/img_root/srv/planet/log:/opt/couchdb/var/log"'
-echo '  planet:'
-echo '    volumes:'
-echo '      - "$(pwd -P)/mnt/img_root/srv/planet/pwd:/usr/share/nginx/html/credentials"'
-echo 'version: "2"'
+echo "services:"
+echo "  couchdb:"
+echo "    volumes:"
+echo "      - \"$(pwd -P)/mnt/img_root/srv/planet/data:/opt/couchdb/data\""
+echo "      - \"$(pwd -P)/mnt/img_root/srv/planet/log:/opt/couchdb/var/log\""
+echo "  planet:"
+echo "    volumes:"
+echo "      - \"$(pwd -P)/mnt/img_root/srv/planet/pwd:/usr/share/nginx/html/credentials\""
+echo "version: \"2\""
 } > volumes.yml
 #docker run -d -p $port:5984 --name bell -v "$(pwd -P)/mnt/img_root/srv/bell/data:/usr/local/var/lib/couchdb" klaemo/couchdb:1.6.1
 
