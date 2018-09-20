9ae10b0f958fec74d1835589d29400881265c4e5
dogi <stefan@unterhauser.name>
2018-09-19 21:24:28 -0400

    next

--- scripts.d/40_planet.sh
+++ scripts.d/40_planet.sh
@@ -9,7 +9,7 @@ cd /root || exit 1
 
 wget https://raw.githubusercontent.com/open-learning-exchange/planet/master/docker/planet.yml
 wget https://raw.githubusercontent.com/open-learning-exchange/planet/master/docker/install.yml
-#wget https://raw.githubusercontent.com/open-learning-exchange/planet/master/docker/volumes.yml
+wget https://raw.githubusercontent.com/open-learning-exchange/planet/master/docker/volumes.yml
 {
 echo "services:"
 echo "  couchdb:"
@@ -20,18 +20,20 @@ echo "  planet:"
 echo "    volumes:"
 echo "      - \"$(pwd -P)/mnt/img_root/srv/$planetdir/pwd:/usr/share/nginx/html/credentials\""
 echo "version: \"2\""
-} > volumes.yml
+} > volumestravis.yml
+
+cp planet.yml install.yml volumes.yml $(pwd -P)/mnt/img_root/root/.
 
 sync; sync; sync
 
-docker-compose -f planet.yml -f volumes.yml -f install.yml -p planet pull
+docker-compose -f planet.yml -f volumestravis.yml -f install.yml -p planet pull
 docker tag treehouses/planet:db-init treehouses/planet:db-init-local
 docker tag treehouses/planet:latest treehouses/planet:local
 
 sync; sync; sync
 
-docker-compose -f planet.yml -f volumes.yml -p planet up -d
+docker-compose -f planet.yml -f volumestravis.yml -p planet up -d
 
 # sync and stop docker
 sync; sync; sync
-docker-compose -f planet.yml -f volumes.yml -p planet stop
+docker-compose -f planet.yml -f volumestravis.yml -p planet stop
