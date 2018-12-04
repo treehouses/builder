19c682a8b7564f92d18e33e0c041aa7b223d2f8e
dogi <dogi@users.noreply.github.com>
2018-12-03 19:10:32 -0500

    update 20_balena.sh (fixes #354) (#355)

--- scripts.d/20_balena.sh
+++ scripts.d/20_balena.sh
@@ -5,23 +5,23 @@ source lib.sh
 echo "Balena installation"
 
 # get the latest version
-releases=$(curl -s https://api.github.com/repos/resin-os/balena/releases/latest | jq -r ".assets[].browser_download_url")
+releases=$(curl -s https://api.github.com/repos/balena-os/balena-engine/releases/latest | jq -r ".assets[].browser_download_url")
 armv6link=$(echo "$releases" | tr " " "\\n" | grep armv6)
 armv7link=$(echo "$releases" | tr " " "\\n" | grep armv7)
 
 # armv7
 wget -c "$armv7link"
-tar xvzf "$(basename "$armv7link")" balena/balena
-mv balena/balena mnt/img_root/usr/bin/balena-armv7l
+tar xvzf "$(basename "$armv7link")" balena-engine/balena-engine
+mv balena-engine/balena-engine mnt/img_root/usr/bin/balena-armv7l
 _op _chroot chown root:root /usr/bin/balena-armv7l
-rm -rf balena/
+rm -rf balena-engine/
 
 # armv6
 wget -c "$armv6link"
-tar xvzf "$(basename "$armv6link")" balena/balena
-mv balena/balena mnt/img_root/usr/bin/balena-armv6l
+tar xvzf "$(basename "$armv6link")" balena-engine/balena-engine
+mv balena-engine/balena-engine mnt/img_root/usr/bin/balena-armv6l
 _op _chroot chown root:root /usr/bin/balena-armv6l
-rm -rf balena/
+rm -rf balena-engine/
 
 
 _op _chroot ln -sr /usr/bin/balena-armv7l /usr/bin/balena
