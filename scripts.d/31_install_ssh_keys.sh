865e2d6585a390d642af010cc97e5ceec5f28d9f
dogi <dogi@users.noreply.github.com>
2019-02-06 22:57:08 -0500

    Update 31_install_ssh_keys.sh

--- scripts.d/31_install_ssh_keys.sh
+++ scripts.d/31_install_ssh_keys.sh
@@ -4,11 +4,17 @@ source lib.sh
 
 ROOT=mnt/img_root
 
+{
+  echo "Host *.onion"
+  echo "  ProxyCommand nc -x localhost:9150 -X 5 %h %p"
+} > config
+
 _install() {
     owner="$1"
     home="$2"
     install -o "$owner" -g "$owner" -m 700 -d "$home/.ssh"
     install -o "$owner" -g "$owner" -m 600 authorized_keys "$home/.ssh"
+    install -o "$owner" -g "$owner" -m 600 config "$home/.ssh"
 }
 
 echo "installing SSH keys for root"
