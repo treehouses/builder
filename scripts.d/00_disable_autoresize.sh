86cd909ebc5069aeb323170f86a0ed74e93b7af9
Ignacio Rodríguez <ignacio@sugarlabs.org>
2019-03-20 21:57:05 -0300

    hmhm

--- scripts.d/00_disable_autoresize.sh
+++ scripts.d/00_disable_autoresize.sh
@@ -2,4 +2,4 @@
 
 source lib.sh
 
-sed -i 's| quiet init=/usr/lib/raspi-config/init_resize.sh||' mnt/img_root/boot/cmdline.txt || die "Could notdisable autoresize"
+#sed -i 's| quiet init=/usr/lib/raspi-config/init_resize.sh||' mnt/img_root/boot/cmdline.txt || die "Could notdisable autoresize"
