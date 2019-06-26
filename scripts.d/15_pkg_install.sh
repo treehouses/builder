6fe7e6ebacb2b994b5ec63c18b87d16518e03c41
dogi <dogi@users.noreply.github.com>
2019-06-26 19:21:47 -0400

    Update 15_pkg_install.sh

--- scripts.d/15_pkg_install.sh
+++ scripts.d/15_pkg_install.sh
@@ -4,7 +4,7 @@ source lib.sh
 
 INSTALL_PACKAGES=(
     avahi-daemon vim lshw iotop screen # essentials
-    docker-ce # docker
+    #docker-ce # docker
     quicksynergy # dogi
     matchbox-keyboard # virtual keyboard
     mdadm initramfs-tools rsync # for RAID1
