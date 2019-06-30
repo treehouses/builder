5e9184a8d6e9bf7e17669582a8f6d8b0f58ab679
dogi <dogi@users.noreply.github.com>
2019-06-29 20:15:28 -0400

    Update 05_add_repos.sh

--- scripts.d/05_add_repos.sh
+++ scripts.d/05_add_repos.sh
@@ -28,10 +28,10 @@ install_stuff() {
 # List of extra APT repositories
 ADD_REPOS=(
     # curl -s https://deb.nodesource.com/gpgkey/nodesource.gpg.key > keys/68576280.key
-    "deb https://deb.nodesource.com/node_8.x stretch main"
-    "deb-src https://deb.nodesource.com/node_8.x stretch main"
+    "deb https://deb.nodesource.com/node_8.x buster main"
+    "deb-src https://deb.nodesource.com/node_8.x buster main"
     # curl -fsSL https://download.docker.com/linux/debian/gpg > keys/0EBFCD88.key
-    "deb [arch=armhf] https://download.docker.com/linux/raspbian buster stable"
+    "deb [arch=armhf] https://download.docker.com/linux/raspbian stretch stable"
     # curl -fsSL http://deb.torproject.org/torproject.org/A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89.asc > keys/A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89.key
     "deb http://deb.torproject.org/torproject.org buster main"
     "deb-src http://deb.torproject.org/torproject.org buster main"
