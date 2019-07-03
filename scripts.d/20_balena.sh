27c3d419f3cccd25bfc667ddfe52c506b0d6e17e
Ignacio Rodríguez <ignacio@sugarlabs.org>
2019-07-03 20:04:52 -0300

    restore previous balena behavior (fixes #428) (#458)

--- scripts.d/20_balena.sh
+++ scripts.d/20_balena.sh
@@ -5,10 +5,7 @@ source lib.sh
 echo "Balena installation"
 
 # get the latest version
-# releases=$(curl -s https://api.github.com/repos/balena-os/balena-engine/releases/latest -H "Authorization: token $GITHUB_KEY" | jq -r ".assets[].browser_download_url")
-
-# FIXME: balena hasn't published the tar files for since v17.12.0
-releases=$(curl -s https://api.github.com/repos/balena-os/balena-engine/releases/13499248 -H "Authorization: token $GITHUB_KEY" | jq -r ".assets[].browser_download_url")
+releases=$(curl -s https://api.github.com/repos/balena-os/balena-engine/releases/latest -H "Authorization: token $GITHUB_KEY" | jq -r ".assets[].browser_download_url")
 armv6link=$(echo "$releases" | tr " " "\\n" | grep armv6)
 armv7link=$(echo "$releases" | tr " " "\\n" | grep armv7)
 
