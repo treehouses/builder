96b93f0d8979e30c2f3c0945e83702e3f1ba0171
dogi <dogi@users.noreply.github.com>
2019-07-11 22:53:10 -0400

    Update 15_pkg_upgrade.sh

--- scripts.d/15_pkg_upgrade.sh
+++ scripts.d/15_pkg_upgrade.sh
@@ -4,7 +4,10 @@ source lib.sh
 
 echo "Holding nodejs Package"
 _op _chroot apt-mark hold nodejs
+_op _chroot apt-mark hold libgdk-pixbuf2.0-dev
 
 echo "Installing Updates"
 _apt update || die "Could not update package sources"
 _apt dist-upgrade || die "Could not upgrade system"
+
+_op _chroot apt-mark unhold libgdk-pixbuf2.0-dev
