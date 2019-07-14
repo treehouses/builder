68d20b35af24f697dff8301fc03460cdc84bb574
dogi <dogi@users.noreply.github.com>
2019-07-14 04:12:51 -0400

    Update 15_pkg_upgrade.sh

--- scripts.d/15_pkg_upgrade.sh
+++ scripts.d/15_pkg_upgrade.sh
@@ -4,10 +4,6 @@ source lib.sh
 
 echo "Holding nodejs Package"
 _op _chroot apt-mark hold nodejs
-
-chromium-browser chromium-browser-l10n chromium-codecs-ffmpeg-extra
-libfm-data libfm-extra4 libfm-gtk-data libfm-gtk4 libfm-modules libfm4
-
 # temporay fix to not break GUI icons and background
 _op _chroot apt-mark hold libfm-data
 _op _chroot apt-mark hold libfm-extra4
