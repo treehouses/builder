23d0f1a0fc42e6aa465619abf84d74964fe68c2f
dogi <dogi@users.noreply.github.com>
2019-07-30 17:16:52 -0400

    Update 15_pkg_upgrade.sh

--- scripts.d/15_pkg_upgrade.sh
+++ scripts.d/15_pkg_upgrade.sh
@@ -5,20 +5,20 @@ source lib.sh
 echo "Holding nodejs Package"
 _op _chroot apt-mark hold nodejs
 # temporay fix to not break GUI icons and background
-_op _chroot apt-mark hold libfm-data
-_op _chroot apt-mark hold libfm-extra4
-_op _chroot apt-mark hold libfm-gtk-data
-_op _chroot apt-mark hold libfm-gtk4
-_op _chroot apt-mark hold libfm-modules
-_op _chroot apt-mark hold libfm4
+#_op _chroot apt-mark hold libfm-data
+#_op _chroot apt-mark hold libfm-extra4
+#_op _chroot apt-mark hold libfm-gtk-data
+#_op _chroot apt-mark hold libfm-gtk4
+#_op _chroot apt-mark hold libfm-modules
+#_op _chroot apt-mark hold libfm4
 
 echo "Installing Updates"
 _apt update || die "Could not update package sources"
 _apt dist-upgrade || die "Could not upgrade system"
 
-_op _chroot apt-mark unhold libfm-data
-_op _chroot apt-mark unhold libfm-extra4
-_op _chroot apt-mark unhold libfm-gtk-data
-_op _chroot apt-mark unhold libfm-gtk4
-_op _chroot apt-mark unhold libfm-modules
-_op _chroot apt-mark unhold libfm4
+#_op _chroot apt-mark unhold libfm-data
+#_op _chroot apt-mark unhold libfm-extra4
+#_op _chroot apt-mark unhold libfm-gtk-data
+#_op _chroot apt-mark unhold libfm-gtk4
+#_op _chroot apt-mark unhold libfm-modules
+#_op _chroot apt-mark unhold libfm4
