fd2fbe6c3cfe1ce360d52f9b9c99cc50ae215f2c
vers4ce <vkafetzopoulos@gmail.com>
2019-08-02 19:11:23 -0400

    Update 15_pkg_upgrade.sh

--- scripts.d/15_pkg_upgrade.sh
+++ scripts.d/15_pkg_upgrade.sh
@@ -5,22 +5,22 @@ source lib.sh
 echo "Holding nodejs Package"
 _op _chroot apt-mark hold nodejs
 # temporay fix to not break GUI icons and background
-_op _chroot apt-mark hold libfm-data
-_op _chroot apt-mark hold libfm-extra4
-_op _chroot apt-mark hold libfm-gtk-data
-_op _chroot apt-mark hold libfm-gtk4
-_op _chroot apt-mark hold libfm-modules
-_op _chroot apt-mark hold libfm4
-_op _chroot apt-mark hold raspberrypi-ui-mods
+#_op _chroot apt-mark hold libfm-data
+#_op _chroot apt-mark hold libfm-extra4
+#_op _chroot apt-mark hold libfm-gtk-data
+#_op _chroot apt-mark hold libfm-gtk4
+#_op _chroot apt-mark hold libfm-modules
+#_op _chroot apt-mark hold libfm4
+#_op _chroot apt-mark hold raspberrypi-ui-mods
 
 echo "Installing Updates"
 _apt update || die "Could not update package sources"
 _apt dist-upgrade || die "Could not upgrade system"
 
-_op _chroot apt-mark unhold libfm-data
-_op _chroot apt-mark unhold libfm-extra4
-_op _chroot apt-mark unhold libfm-gtk-data
-_op _chroot apt-mark unhold libfm-gtk4
-_op _chroot apt-mark unhold libfm-modules
-_op _chroot apt-mark unhold libfm4
-_op _chroot apt-mark unhold raspberrypi-ui-mods
+#_op _chroot apt-mark unhold libfm-data
+#_op _chroot apt-mark unhold libfm-extra4
+#_op _chroot apt-mark unhold libfm-gtk-data
+#_op _chroot apt-mark unhold libfm-gtk4
+#_op _chroot apt-mark unhold libfm-modules
+#_op _chroot apt-mark unhold libfm4
+#_op _chroot apt-mark unhold raspberrypi-ui-mods
