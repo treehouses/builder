be1ecd4daa110f627e76427415e210709dc313c1
vers4ce <vkafetzopoulos@gmail.com>
2019-08-03 20:36:28 -0400

    Update 30_bluetooth.sh

--- scripts.d/30_bluetooth.sh
+++ scripts.d/30_bluetooth.sh
@@ -4,7 +4,7 @@ source lib.sh
 
 curl "https://raw.githubusercontent.com/treehouses/control/master/server.py" -o mnt/img_root/usr/local/bin/bluetooth-server.py
 
-echo "Switching bluetooth device class to 0x20108 - computer"
-sed -i -e 's/#Class = .*/Class = 0x20108/g' mnt/img_root/etc/bluetooth/main.conf
+echo "Switching bluetooth device class to 0x00100 - computer"
+sed -i -e 's/#Class = .*/Class = 0x00100/g' mnt/img_root/etc/bluetooth/main.conf
 
 _pip3_install pybluez
