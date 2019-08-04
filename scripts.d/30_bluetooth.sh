aa5f8e2f81dc33eb7ff7e9a630e2e303186b623e
vers4ce <vkafetzopoulos@gmail.com>
2019-08-03 21:03:28 -0400

    Update 30_bluetooth.sh

--- scripts.d/30_bluetooth.sh
+++ scripts.d/30_bluetooth.sh
@@ -4,7 +4,13 @@ source lib.sh
 
 curl "https://raw.githubusercontent.com/treehouses/control/master/server.py" -o mnt/img_root/usr/local/bin/bluetooth-server.py
 
-echo "Switching bluetooth device class to 0x00100 - computer"
+echo "Switching bluetooth device class to 0x20108 - networking server"
 sed -i -e 's/#Class = .*/Class = 0x00100/g' mnt/img_root/etc/bluetooth/main.conf
 
 _pip3_install pybluez
+
+# Disabling the hostname BlueZ plugin that resets the device
+bluetoothd -P hostname
+
+# Setting the same device class with hciconfig
+hciconfig hci0 class 020108
