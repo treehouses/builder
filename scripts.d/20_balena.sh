1e34459d7a1a1403cac69b98d423f4ba7df07172
dogi <dogi@users.noreply.github.com>
2019-08-04 16:34:58 -0400

    Update 20_balena.sh

--- scripts.d/20_balena.sh
+++ scripts.d/20_balena.sh
@@ -12,25 +12,25 @@ armv7link=$(echo "$releases" | tr " " "\\n" | grep armv7)
 # armv7
 wget -c "$armv7link"
 tar xvzf "$(basename "$armv7link")" balena-engine/balena-engine
-mv balena-engine/balena-engine mnt/img_root/usr/bin/balena-armv7l
-_op _chroot chown root:root /usr/bin/balena-armv7l
+mv balena-engine/balena-engine mnt/img_root/usr/bin/balena-engine-armv7l
+_op _chroot chown root:root /usr/bin/balena-engine-armv7l
 rm -rf balena-engine/
 
 # armv6
 wget -c "$armv6link"
 tar xvzf "$(basename "$armv6link")" balena-engine/balena-engine
-mv balena-engine/balena-engine mnt/img_root/usr/bin/balena-armv6l
-_op _chroot chown root:root /usr/bin/balena-armv6l
+mv balena-engine/balena-engine mnt/img_root/usr/bin/balena-engine-armv6l
+_op _chroot chown root:root /usr/bin/balena-engine-armv6l
 rm -rf balena-engine/
 
 
-_op _chroot ln -sr /usr/bin/balena-armv7l /usr/bin/balena
-_op _chroot ln -sr /usr/bin/balena /usr/bin/balena-containerd
-_op _chroot ln -sr /usr/bin/balena /usr/bin/balena-containerd-ctr
-_op _chroot ln -sr /usr/bin/balena /usr/bin/balena-containerd-shim
-_op _chroot ln -sr /usr/bin/balena /usr/bin/balenad
-_op _chroot ln -sr /usr/bin/balena /usr/bin/balena-proxy
-_op _chroot ln -sr /usr/bin/balena /usr/bin/balena-runc
+_op _chroot ln -sr /usr/bin/balena-engine-armv7l /usr/bin/balena-engine
+_op _chroot ln -sr /usr/bin/balena-engine /usr/bin/balena-engine-containerd
+_op _chroot ln -sr /usr/bin/balena-engine /usr/bin/balena-engine-containerd-ctr
+_op _chroot ln -sr /usr/bin/balena-engine /usr/bin/balena-engine-containerd-shim
+_op _chroot ln -sr /usr/bin/balena-engine /usr/bin/balena-engine-daemon
+_op _chroot ln -sr /usr/bin/balena-engine /usr/bin/balena-engine-proxy
+_op _chroot ln -sr /usr/bin/balena-engine /usr/bin/balena-engine-runc
 
 _op _chroot groupadd balena
 _op _chroot usermod -aG balena pi
