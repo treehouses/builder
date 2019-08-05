09ca24c19db07d58360424eebb9cd63f79dedb1d
dogi <dogi@users.noreply.github.com>
2019-08-04 22:51:48 -0400

    Update 20_balena.sh

--- scripts.d/20_balena.sh
+++ scripts.d/20_balena.sh
@@ -23,8 +23,7 @@ mv balena-engine/balena-engine mnt/img_root/usr/bin/balena-engine-armv6l
 _op _chroot chown root:root /usr/bin/balena-engine-armv6l
 rm -rf balena-engine/
 
-
-_op _chroot ln -sr /usr/bin/balena-engine-armv7l /usr/bin/balena-engine
+_op _chroot touch /usr/bin/balena-engine
 _op _chroot ln -sr /usr/bin/balena-engine /usr/bin/balena-engine-containerd
 _op _chroot ln -sr /usr/bin/balena-engine /usr/bin/balena-engine-containerd-ctr
 _op _chroot ln -sr /usr/bin/balena-engine /usr/bin/balena-engine-containerd-shim
@@ -32,6 +31,8 @@ _op _chroot ln -sr /usr/bin/balena-engine /usr/bin/balena-engine-daemon
 _op _chroot ln -sr /usr/bin/balena-engine /usr/bin/balena-engine-proxy
 _op _chroot ln -sr /usr/bin/balena-engine /usr/bin/balena-engine-runc
 _op _chroot ln -sr /usr/bin/balena-engine /usr/bin/balena
+_op _chroot rm /usr/bin/balena-engine
+_op _chroot ln -sr /usr/bin/balena-engine-armv7l /usr/bin/balena-engine
 
 _op _chroot groupadd balena
 _op _chroot usermod -aG balena pi
