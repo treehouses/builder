d516137e7a0158c811b6894732045303a2fca5b3
Iris B <irisbeharaj@gmail.com>
2019-09-07 05:18:19 +0200

    add terraform (fixes #535) (#538)

---
+++ scripts.d/32_terraform.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+wget https://releases.hashicorp.com/terraform/0.12.7/terraform_0.12.7_linux_arm.zip
+unzip terraform_0.12.7_linux_arm.zip
+
+mkdir -p mnt/img_root/usr/local/bin/
+mv terraform mnt/img_root/usr/local/bin/.
