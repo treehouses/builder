7087c242f5adfb6e7a601410ee295d9ad23462ef
lillyxxx <33206707+lillyxxx@users.noreply.github.com>
2019-09-18 16:07:55 -0700

    update upload.sh (fixes #522)

--- .travis/upload.sh
+++ .travis/upload.sh
@@ -5,8 +5,12 @@ source .travis/utils.sh
 echo "Uploading image to dev"
 rsync -P -e .travis/ssh.sh "$image_gz" "$image_sha1" deploy@download.ole.org:/data/images
 
+echo "Download the uploaded image: http://download.treehouses.io/$image_gz"
+
 if release_is_number; then
     echo "Marking release as latest image"
     # For some reason the cd has no effect if it's the first command? WTF.
     .travis/ssh.sh deploy@download.ole.org sh -c ":; cd /data/images; ln -sf $name.img.gz latest.img.gz; ln -sf $name.img.gz.sha1 latest.img.gz.sha1"
+    
+    echo "Download the latest image: http://download.treehouses.io/latest.img.gz"
 fi
