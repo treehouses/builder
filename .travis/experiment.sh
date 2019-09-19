58585e6c462eeee5541a652e5741c5e6f1f34fd3
lilyxxx <33206707+lillyxxx@users.noreply.github.com>
2019-09-18 20:24:20 -0700

    update image path

--- .travis/experiment.sh
+++ .travis/experiment.sh
@@ -3,8 +3,10 @@
 source .travis/utils.sh
 
 build_type="branch"
+image_dir = "experiment/"
 if release_is_number; then
     build_type="latest"
+    image_dir=""
 fi
 export experiment="$build_type"
-export img_name="$image_gz"
+export image_path="$image_dir$image_gz"
