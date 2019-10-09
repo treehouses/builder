f182758913a74519af6fd94866d8e7ce5fb4558c
Anna X <vmnet8@gmail.com>
2019-10-09 03:51:26 +0000

    fix bash completion

--- scripts.d/30_npminstall.sh
+++ scripts.d/30_npminstall.sh
@@ -8,5 +8,7 @@ mkdir node_root
 npm config set prefix "$PWD/node_root"
 npm install --unsafe-perm -g @treehouses/cli@1.8.5
 (cd node_root || exit 1; tar c .) | (cd mnt/img_root/usr || exit 1; tar x)
+cd ..
+ln -sr usr/lib/node_modules/@treehouses/cli/_treehouses etc/bash_completion.d/_treehouses
 npm config delete prefix
 rm -rf node_root
