02ac79b8df6a7b0e9a52a600d7920dce6e7c5c1f
dogi <dogi@users.noreply.github.com>
2019-10-16 23:09:40 -0400

    Revert "add terraform (fixes #570) (#571)"
    This reverts commit 2ba34aa772d40e44fea649d565083eb32c9782e1.

--- scripts.d/32_terraform.sh
+++ scripts.d/32_terraform.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-wget https://releases.hashicorp.com/terraform/0.12.9/terraform_0.12.10_linux_arm.zip
-unzip terraform_0.12.10_linux_arm.zip
+wget https://releases.hashicorp.com/terraform/0.12.9/terraform_0.12.9_linux_arm.zip
+unzip terraform_0.12.9_linux_arm.zip
 
 mkdir -p mnt/img_root/usr/local/bin/
 mv terraform mnt/img_root/usr/local/bin/.
