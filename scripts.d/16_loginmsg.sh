5d480503866f703f16d25081e2c47ebb23dc3dbf
root <root@travis-job-59e9db0a-1336-45b6-8cd6-cc9f5b586a12>
2019-10-21 22:43:28 +0000

    updated url

--- scripts.d/16_loginmsg.sh
+++ scripts.d/16_loginmsg.sh
@@ -5,12 +5,12 @@ chmod u+x /etc/motd
 
 echo "" >> $motd
 echo "Welcome to the treehouses project!" >> $motd
-echo "" >> $motd
+echo " " >> $motd
 echo "The Open Learning Exchange is a social benefit and for-purpose organization based in Cambridge, Massachusetts." >> $motd
 echo "Our mission is to provide universal quality education using open source materials and technology to overcome educational barriers in third world countries." >> $motd
-echo "" >> $motd
+echo " " >> $motd
 echo "For more information, please visit: http://treehouses.io/" >> $motd
 echo "" >> $motd
 echo "To begin using treehouses type \`treehouses help\` " >> $motd
-echo "" >> $motd
+echo " " >> $motd
 
