3c25bda8f82b51c33c53b6b42430953cec1384a5
root <root@travis-job-59e9db0a-1336-45b6-8cd6-cc9f5b586a12>
2019-10-21 22:48:21 +0000

    combined echo commands

--- scripts.d/16_loginmsg.sh
+++ scripts.d/16_loginmsg.sh
@@ -3,14 +3,15 @@
 motd=/etc/motd
 chmod u+x /etc/motd
 
-echo "" >> $motd
-echo "Welcome to the treehouses project!" >> $motd
-echo " " >> $motd
-echo "The Open Learning Exchange is a social benefit and for-purpose organization based in Cambridge, Massachusetts." >> $motd
-echo "Our mission is to provide universal quality education using open source materials and technology to overcome educational barriers in third world countries." >> $motd
-echo " " >> $motd
-echo "For more information, please visit: http://treehouses.io/" >> $motd
-echo "" >> $motd
-echo "To begin using treehouses type \`treehouses help\` " >> $motd
-echo " " >> $motd
+{
+echo " "
+echo "Welcome to the treehouses project!"
+echo " "
+echo "The Open Learning Exchange is a social benefit and for-purpose organization based in Cambridge, Massachusetts."
+echo "Our mission is to provide universal quality education using open source materials and technology to overcome educational barriers in third world countries."
+echo " "
+echo "For more information, please visit: http://treehouses.io/"
+echo " "
+echo "To begin using treehouses type \`treehouses help\` "
+echo " " } >> $motd
 
