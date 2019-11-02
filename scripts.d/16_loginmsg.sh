500ca387bdc5341042c0021f7b53cf279ce5e634
pattanawadee88 <52762574+pattanawadee88@users.noreply.github.com>
2019-11-01 21:10:29 -0400

    Update 16_loginmsg.sh

--- scripts.d/16_loginmsg.sh
+++ scripts.d/16_loginmsg.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-motd=/etc/motd
+motd=mnt/img_root/etc/motd
 
 {
 echo " "
+echo" _                     _                                       _"
+echo"| |_  _ __  ___   ___ | |__    ___   _   _  ___   ___  ___    (_)  ___"
+echo"| __|| '__|/ _ \ / _ \| '_ \  / _ \ | | | |/ __| / _ \/ __|   | | / _ \"
+echo"| |_ | |  |  __/|  __/| | | || (_) || |_| |\__ \|  __/\__ \ _ | || (_) |"
+echo" \__||_|   \___| \___||_| |_| \___/  \__,_||___/ \___||___/(_)|_|\___/"
+
 echo "Welcome to the treehouses project!"
 echo " "
 echo "The Open Learning Exchange is a social benefit and for-purpose organization based in Cambridge, Massachusetts."
@@ -14,4 +20,3 @@ echo " "
 echo "To begin using treehouses type \`treehouses help\` "
 echo " " 
 } >> $motd
-
