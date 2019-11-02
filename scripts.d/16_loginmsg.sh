5feefe716e110d07fc3ee248329b65c2a0ae12d5
pattanawadee88 <52762574+pattanawadee88@users.noreply.github.com>
2019-11-01 21:29:26 -0400

    Update 16_loginmsg.sh

--- scripts.d/16_loginmsg.sh
+++ scripts.d/16_loginmsg.sh
@@ -4,12 +4,11 @@ motd=mnt/img_root/boot/etc/motd
 
 {
 echo " "
-echo" _                     _                                       _"
-echo"| |_  _ __  ___   ___ | |__    ___   _   _  ___   ___  ___    (_)  ___"
-echo"| __|| '__|/ _ \ / _ \| '_ \  / _ \ | | | |/ __| / _ \/ __|   | | / _ \"
-echo"| |_ | |  |  __/|  __/| | | || (_) || |_| |\__ \|  __/\__ \ _ | || (_) |"
-echo" \__||_|   \___| \___||_| |_| \___/  \__,_||___/ \___||___/(_)|_| \___/"
-
+echo " _                     _                                       _"
+echo "| |_  _ __  ___   ___ | |__    ___   _   _  ___   ___  ___    (_)  ___"
+echo "| __|| '__|/ _ \ / _ \| '_ \  / _ \ | | | |/ __| / _ \/ __|   | | / _ \"
+echo "| |_ | |  |  __/|  __/| | | || (_) || |_| |\__ \|  __/\__ \ _ | || (_) |"
+echo " \__||_|   \___| \___||_| |_| \___/  \__,_||___/ \___||___/(_)|_| \___/"
 echo "Welcome to the treehouses project!"
 echo " "
 echo "The Open Learning Exchange is a social benefit and for-purpose organization based in Cambridge, Massachusetts."
