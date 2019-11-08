2bc66833b710c341226556faa6550cd86a0c6a2a
pattanawadee88 <52762574+pattanawadee88@users.noreply.github.com>
2019-11-08 13:47:30 -0500

    Update 16_loginmsg.sh

--- scripts.d/16_loginmsg.sh
+++ scripts.d/16_loginmsg.sh
@@ -2,20 +2,21 @@
 
 motd=mnt/img_root/etc/motd
 green="\033[1;32m"
-brown="\033[0;33m"
+red="\033[0;31m"
+reset="\033[0m"
 {
-  echo -e "${brown}#################################################################################"
-  echo -e "#${green}     _                     _                                       _           ${brown}#"
-  echo -e "#${green}    | |_  _ __  ___   ___ | |__    ___   _   _  ___   ___  ___    (_)  ___     ${brown}#"
-  echo -e "#${green}    | __|| '__|/ _ \\ / _ \\| '_ \\  / _ \\ | | | |/ __| / _ \\/ __|   | | / _ \\    ${brown}#"
-  echo -e "#${green}    | |_ | |  |  __/|  __/| | | || (_) || |_| |\\__ \\|  __/\\__ \\ _ | || (_) |   ${brown}#"
-  echo -e "#${green}     \\__||_|   \\___| \\___||_| |_| \\___/  \\__,_||___/ \\___||___/(_)|_| \\___/    ${brown}#"
-  echo -e "#${brown}                                                                               #"
-  echo -e "#${brown}                    Welcome to the treehouses project!                         ${brown}#"
-  echo -e "#${brown}         For more information, please visit: http://treehouses.io/             ${brown}#"
-  echo -e "#${brown}            To begin using treehouses type 'treehouses help'                   ${brown}#"
-  echo -e "#${brown}   Please feel free to contact us in the 'https://gitter.im/treehouses/Lobby'  ${brown}#"
-  echo -e "${brown}#################################################################################"
+  echo -e "${red}#################################################################################"
+  echo -e "#${green}   _                     _                                       _               ${red}#"
+  echo -e "#${green}  | |_  _ __  ___   ___ | |__    ___   _   _  ___   ___  ___    (_)  ___         ${red}#"
+  echo -e "#${green}  | __|| '__|/ _ \\ / _ \\| '_ \\  / _ \\ | | | |/ __| / _ \\/ __|   | | / _ \\        ${red}#"
+  echo -e "#${green}  | |_ | |  |  __/|  __/| | | || (_) || |_| |\\__ \\|  __/\\__ \\ _ | || (_) |       ${red}#"
+  echo -e "#${green}   \\__||_|   \\___| \\___||_| |_| \\___/  \\__,_||___/ \\___||___/(_)|_| \\___/        ${red}#"
+  echo -e "#${red}                                                                               #"
+  echo -e "#${red}                    Welcome to the treehouses project!                         ${red}#"
+  echo -e "#${red}         For more information, please visit: http://treehouses.io/             ${red}#"
+  echo -e "#${red}            To begin using treehouses type 'treehouses help'                   ${red}#"
+  echo -e "#${red}   Please feel free to contact us in the 'https://gitter.im/treehouses/Lobby'  ${red}#"
+  echo -e "${red}#################################################################################${reset}"
  
 } > $motd
 
