4fbd58e59182f059206dfe1d6d2bb1af951ae5a4
Vassilis Kafetzopoulos <vkafetzopoulos@gmail.com>
2019-12-26 17:43:49 +0200

    Update 21_pwnagotchi.sh

--- scripts.d/21_pwnagotchi.sh
+++ scripts.d/21_pwnagotchi.sh
@@ -1,3 +1,71 @@
 #!/bin/bash
 
 source lib.sh
+
+# Install bettercap
+wget "https://github.com/bettercap/bettercap/releases/download/v2.26.1/bettercap_linux_armhf_v2.26.1.zip"
+unzip bettercap_linux_armhf_v2.26.1.zip
+mv bettercap /usr/bin/
+bettercap -eval "caplets.update; ui.update; quit"
+
+cat <<EOFA > /etc/systemd/system/bettercap.service
+[Unit]
+Description=bettercap api.rest service.
+Documentation=https://bettercap.org
+Wants=network.target
+After=pwngrid.service
+
+[Service]
+Type=simple
+PermissionsStartOnly=true
+ExecStart=/usr/bin/bettercap-launcher
+Restart=always
+RestartSec=30
+
+[Install]
+WantedBy=multi-user.target
+EOFA
+
+cat <<EOFB > /usr/bin/bettercap-launcher
+#!/usr/bin/env bash
+/usr/bin/monstart
+if [[ $(ifconfig | grep usb0 | grep RUNNING) ]] || [[ $(cat /sys/class/net/eth0/carrier) ]]; then
+  # if override file exists, go into auto mode
+  if [ -f /root/.pwnagotchi-auto ]; then
+    /usr/bin/bettercap -no-colors -caplet pwnagotchi-auto -iface mon0
+  else
+    /usr/bin/bettercap -no-colors -caplet pwnagotchi-manual -iface mon0
+  fi
+else
+  /usr/bin/bettercap -no-colors -caplet pwnagotchi-auto -iface mon0
+fi
+EOFB
+
+# Install pwngrid
+wget "https://github.com/evilsocket/pwngrid/releases/download/v1.10.3/pwngrid_linux_armhf_v1.10.3.zip"
+unzip pwngrid_linux_armhf_v1.10.3.zip
+mv pwngrid /usr/bin/
+pwngrid -generate -keys /etc/pwnagotchi
+
+cat <<EOFC > /etc/systemd/system/pwngrid-peer.service
+[Unit]
+Description=pwngrid peer service.
+Documentation=https://pwnagotchi.ai
+Wants=network.target
+
+[Service]
+Type=simple
+PermissionsStartOnly=true
+ExecStart=/usr/bin/pwngrid -keys /etc/pwnagotchi -address 127.0.0.1:8666 -client-token /root/.api-enrollment.json -wait -log /var/log/pwngrid-peer.log -iface mon0
+Restart=always
+RestartSec=30
+
+[Install]
+WantedBy=multi-user.target
+EOFC
+
+# Install pwnagotchi
+wget "https://github.com/evilsocket/pwnagotchi/archive/v1.4.3.zip"
+unzip v1.4.3.zip
+cd pwnagotchi-1.4.3
+pip3 install .
