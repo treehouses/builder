b71335217e9c76ed513bc5745bb0b1ef06e9edd7
kjong <36876018+kjong@users.noreply.github.com>
2020-01-02 22:38:09 -0800

    Update 33_usb.sh

--- scripts.d/33_usb.sh
+++ scripts.d/33_usb.sh
@@ -3,7 +3,7 @@
 source lib.sh
 
 wget https://raw.githubusercontent.com/codazoda/hub-ctrl.c/master/hub-ctrl.c
-gcc -o hub-ctrl hub-ctrl.c -lusb
+_op _chroot gcc -o hub-ctrl hub-ctrl.c -lusb
 
 mkdir -p mnt/img_root/usr/local/bin/
 mv hub-ctrl mnt/img_root/usr/local/bin/.
