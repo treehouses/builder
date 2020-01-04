c244f46c6616fb5277ab6e3dc6078ea7f06ae22a
dogi <dogi@users.noreply.github.com>
2020-01-04 00:47:54 -0500

    @treehouses/cli@1.12.0 (#613)

--- scripts.d/30_npminstall.sh
+++ scripts.d/30_npminstall.sh
@@ -6,7 +6,7 @@ echo "Installing @treehouses/cli script"
 
 mkdir node_root
 npm config set prefix "$PWD/node_root"
-npm install --unsafe-perm -g @treehouses/cli@1.11.1
+npm install --unsafe-perm -g @treehouses/cli@1.12.0
 (cd node_root || exit 1; tar c .) | (cd mnt/img_root/usr || exit 1; tar x)
 echo "$PWD"
 ln -sr "$PWD/mnt/img_root/usr/lib/node_modules/@treehouses/cli/_treehouses" "$PWD/mnt/img_root/etc/bash_completion.d/_treehouses"
