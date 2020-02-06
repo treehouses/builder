93067eef52cdeb39782c857b090eeb93c269b2fa
LordJashin32 <tristanlangford32@gmail.com>
2020-02-05 20:06:14 -0500

    run to the hills, for your lifeee

---
+++ scripts.d/34_blocker.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cd "$PWD/mnt/img_root/usr/lib/node_modules/@treehouses/cli/"
+wget -q -O 1_hosts https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts
+wget -q -O 2_hosts https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/porn/hosts
+wget -q -O 3_hosts https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/gambling-porn/hosts
+wget -q -O 4_hosts https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/fakenews-gambling-porn/hosts
+wget -q -O 5_hosts https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/fakenews-gambling-porn-social/hosts
+cd mnt/img_root/usr
