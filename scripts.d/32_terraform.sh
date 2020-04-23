657369caa580667443a5cb5b7cff2e95b7a65925
root <root@travis-job-a8a11abd-e324-4158-ac93-653cd844eb0c>
2020-04-22 18:33:04 -0700

    update terraform

--- scripts.d/32_terraform.sh
+++ scripts.d/32_terraform.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-wget https://releases.hashicorp.com/terraform/0.12.10/terraform_0.12.10_linux_arm.zip
-unzip terraform_0.12.10_linux_arm.zip
+wget https://releases.hashicorp.com/terraform/0.12.24/terraform_0.12.24_linux_arm.zip
+
+unzip terraform_0.12.24_linux_arm.zip
+
 
 mkdir -p mnt/img_root/usr/local/bin/
+
 mv terraform mnt/img_root/usr/local/bin/.
+
