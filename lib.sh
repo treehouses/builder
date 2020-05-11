44a83b429a9c3557f07f0c7c3dbfeda1104d68f3
Lawrence Lin <35390215+law-lin@users.noreply.github.com>
2020-05-11 17:42:36 -0400

    re-try 3 times on package install (fixes #674) (#688)
    Co-authored-by: dogi <dogi@users.noreply.github.com>

--- lib.sh
+++ lib.sh
@@ -17,7 +17,7 @@ function _chroot {
 }
 
 function _apt {
-    _op _chroot apt-get -o Acquire::ForceIPv4=true -qq "$@"
+    _op _chroot apt-get -o Acquire::ForceIPv4=true -o APT::Acquire::Retries=3 -qq "$@"
 }
 
 function _pip3_install {
