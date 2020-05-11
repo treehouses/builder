6bb627f0eeb0a02095843d6e9c3fd85a2f02c278
law-lin <lawrencelin101@gmail.com>
2020-05-11 15:56:13 -0400

    fix _apt

--- lib.sh
+++ lib.sh
@@ -17,7 +17,7 @@ function _chroot {
 }
 
 function _apt {
-    _op _chroot apt-get -o APT::Acquire::Retries=3 install -y $PACKAGES
+    _op _chroot apt-get -o Acquire::ForceIPv4=true -o APT::Acquire::Retries=3 -qq "$@"
 }
 
 function _pip3_install {
