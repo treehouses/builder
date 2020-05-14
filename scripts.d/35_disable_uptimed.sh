5b44e7d568a38dc97fb63fda538bd98a150b0873
law-lin <lawrencelin101@gmail.com>
2020-05-13 21:34:48 -0400

    fix comment

--- scripts.d/35_disable_uptimed.sh
+++ scripts.d/35_disable_uptimed.sh
@@ -1,2 +1,1 @@
-# disables the uptimed daemon from running in the background on boot; still allows manual start/stop
-systemctl disable uptimed
+systemctl disable uptimed # disables the uptimed daemon from running in the background on boot; still allows manual start/stop
