649ab3363d7bf84e35a3a8785101a542ea46e852
dogi <dogi@users.noreply.github.com>
2020-05-16 03:26:10 -0400

    Update 35_disable_uptimed.sh

--- scripts.d/35_disable_uptimed.sh
+++ scripts.d/35_disable_uptimed.sh
@@ -2,4 +2,4 @@
 
 source lib.sh
 
-_op _chroot systemctl disable uptimed # disables the uptimed daemon from running in the background on boot; still allows manual start/stop
+_op _chroot systemctl disable uptimed
