2efc6a6cefaa0ba333829b2805705e3abd8ed5fc
law-lin <lawrencelin101@gmail.com>
2020-05-23 03:01:52 -0400

    removing all symbolic links

--- scripts.d/35_disable_uptimed.sh
+++ scripts.d/35_disable_uptimed.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-source lib.sh
-
-_op _chroot update-rc.d -f /etc/init.d/uptimed.sh remove
+rm -f mnt/img_root/etc/rc0.d/K01uptimed
+rm -f mnt/img_root/etc/rc1.d/K01uptimed
+rm -f mnt/img_root/etc/rc2.d/S01uptimed
+rm -f mnt/img_root/etc/rc3.d/S01uptimed
+rm -f mnt/img_root/etc/rc4.d/S01uptimed
+rm -f mnt/img_root/etc/rc5.d/S01uptimed
+rm -f mnt/img_root/etc/rc6.d/K01uptimed
