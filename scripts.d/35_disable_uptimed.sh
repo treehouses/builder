fea4b661f591b26b7eb8e6fb1d0655d214d3d2ac
ComputerOnFire <computeronfire@protonmail.com>
2020-05-30 00:18:56 -0500

    remove symlink

--- scripts.d/35_disable_uptimed.sh
+++ scripts.d/35_disable_uptimed.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 
-rm -f mnt/img_root/etc/rc0.d/K01uptimed
-rm -f mnt/img_root/etc/rc1.d/K01uptimed
-rm -f mnt/img_root/etc/rc2.d/S01uptimed
-rm -f mnt/img_root/etc/rc3.d/S01uptimed
-rm -f mnt/img_root/etc/rc4.d/S01uptimed
-rm -f mnt/img_root/etc/rc5.d/S01uptimed
-rm -f mnt/img_root/etc/rc6.d/K01uptimed
+rm -f mnt/img_root/etc/systemd/system/multi-user.target.wants/uptimed.service
