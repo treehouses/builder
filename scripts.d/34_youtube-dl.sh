ff87b76b45d25825b449663f0235858df8bf2273
Sydney Aldo <58101855+sydneyaldo@users.noreply.github.com>
2020-06-01 17:57:36 +0100

    Update 34_youtube-dl.sh

--- scripts.d/34_youtube-dl.sh
+++ scripts.d/34_youtube-dl.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-curl -L https://yt-dl.org/downloads/latest/youtube-dl -o mnt/img_root/usr/local/bin/youtube-dl
+ls -al mnt/img_root/usr/local/bin/
+curl -L https://yt-dl.org/downloads/latest/youtube-dl -k -o mnt/img_root/usr/local/bin/youtube-dl 
+ls -al mnt/img_root/usr/local/bin/youtube-dl
 chmod a+rx mnt/img_root/usr/local/bin/youtube-dl
