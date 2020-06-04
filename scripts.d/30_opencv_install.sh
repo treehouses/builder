2d3effdedefda95d9140a7358485179bca847756
ComputerOnFire <computeronfire@protonmail.com>
2020-06-03 21:06:35 -0500

    move dependencies to 16_pkg_install.sh

--- scripts.d/30_opencv_install.sh
+++ scripts.d/30_opencv_install.sh
@@ -1,23 +1,6 @@
 #!/bin/sh
 source lib.sh
 
-echo "Installing OpenCV dependencies"
-#sudo apt-get update && sudo apt-get upgrade
-#sudo apt-get install -y build-essential cmake pkg-config
-#sudo apt-get install -y libjpeg-dev libtiff5-dev libjasper-dev libpng-dev
-#sudo apt-get install -y libavcodec-dev libavformat-dev libswscale-dev libv4l-dev
-#sudo apt-get install -y libxvidcore-dev libx264-dev
-#sudo apt-get install -y libfontconfig1-dev libcairo2-dev
-#sudo apt-get install -y libgdk-pixbuf2.0-dev libpango1.0-dev
-#sudo apt-get install -y libgtk2.0-dev libgtk-3-dev
-#sudo apt-get install -y libatlas-base-dev gfortran
-#sudo apt-get install -y libhdf5-dev libhdf5-serial-dev libhdf5-103
-#sudo apt-get install -y libjasper1
-#sudo apt-get install -y libatlas-base-dev
-#sudo apt-get install -y libqtgui4 libqtwebkit4 libqt4-test python3-pyqt5
-#sudo apt-get install -y python3-dev
-_op _chroot apt-get install libhdf5-dev libatlas-base-dev libjasper1 libqt4-test
-
 echo "Installing OpenCV and NumPy on a virtual envirnonment"
 _op _chroot wget -N https://bootstrap.pypa.io/get-pip.py
 _op _chroot python get-pip.py
