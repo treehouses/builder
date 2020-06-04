e4642b7d36449ac49b48b436ff5dbdf031367f64
ComputerOnFire <computeronfire@protonmail.com>
2020-06-03 20:34:40 -0500

    add install script

---
+++ scripts.d/30_opencv_install.sh
@@ -0,0 +1,61 @@
+echo "getting necessary packages"
+#sudo apt-get update && sudo apt-get upgrade
+#sudo apt-get install -y build-essential cmake pkg-config
+#sudo apt-get install -y libjpeg-dev libtiff5-dev libjasper-dev libpng-dev
+#sudo apt-get install -y libavcodec-dev libavformat-dev libswscale-dev libv4l-dev
+#sudo apt-get install -y libxvidcore-dev libx264-dev
+#sudo apt-get install -y libfontconfig1-dev libcairo2-dev
+#sudo apt-get install -y libgdk-pixbuf2.0-dev libpango1.0-dev
+#sudo apt-get install -y libgtk2.0-dev libgtk-3-dev
+#sudo apt-get install -y libatlas-base-dev gfortran
+#sudo apt-get install -y libhdf5-dev libhdf5-serial-dev libhdf5-103
+#sudo apt-get install -y libjasper1
+#sudo apt-get install -y libatlas-base-dev
+#sudo apt-get install -y libqtgui4 libqtwebkit4 libqt4-test python3-pyqt5
+#sudo apt-get install -y python3-dev
+_op _chroot apt-get install libhdf5-dev libatlas-base-dev libjasper1 libqt4-test
+
+echo "Installing OpenCV and NumPy on a virtual envirnonment"
+_op _chroot wget -N https://bootstrap.pypa.io/get-pip.py
+_op _chroot python get-pip.py
+_op _chroot python3 get-pip.py
+_op _chroot rm -rf ~/.cache/pip
+_op _chroot python3 -m pip install virtualenv
+_op _chroot python3 -m pip install virtualenvwrapper
+
+echo "# virtualenv and virtualenvwrapper
+export WORKON_HOME=$HOME/.virtualenvs
+export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python3
+export LD_PRELOAD=/usr/lib/arm-linux-gnueabihf/libatomic.so.1.2.0
+source /usr/local/bin/virtualenvwrapper.sh" >> ~/.bashrc
+
+source ~/.bashrc
+
+sudo bash -c 'source /usr/local/bin/virtualenvwrapper.sh;mkvirtualenv cv -p python3;source ~/.virtualenvs/cv/bin/activate'
+
+_op _chroot pip3 install "picamera[array]"
+_op _chroot pip3 install opencv-python
+_op _chroot pip3 install opencv-contrib-python
+_op _chroot pip3 install --upgrade imutils
+_op _chroot deactivate
+_op _chroot wget -N https://github.com/opencv/opencv/tree/master/data/haarcascades/haarcascade_frontalface_default.xml
+
+
+echo "# Enable Camera"
+_op _chroot raspi-config nonint do_camera 0
+
+echo "Verify your install worked by the following commands:"
+echo ""
+echo "cd ~"
+echo "source /usr/local/bin/virtualenvwrapper.sh"
+echo "mkvirtualenv cv -p python3"
+echo "source ~/.virtualenvs/cv/bin/activate"
+echo "python3"
+echo "import cv2"
+echo "cv2.__version__"
+echo ""
+echo "You should see \"4.1.1\" or higher if it worked."
+echo "If installation is successful, you'll need to restart to get the camera working."
+echo "Upon reboot, run the following to verify the camera works: "
+echo ""
+echo "sudo raspistill -e png -n -o /home/pi/Pictures/activation_test.png"
