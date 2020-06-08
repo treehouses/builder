f4ec422566e337ecd3ba6b5923415a83caf617ae
ComputerOnFire <computeronfire@protonmail.com>
2020-06-08 05:29:41 -0500

    fix compiler version

--- scripts.d/20_tiv.sh
+++ scripts.d/20_tiv.sh
@@ -2,5 +2,5 @@
 
 git clone https://github.com/stefanhaustein/TerminalImageViewer.git
 cd TerminalImageViewer/src/main/cpp
-CXX=arm-linux-gnueabihf-gcc make
+CXX=arm-linux-gnueabihf-g++ make
 mv tiv mnt/img_root/usr/local/bin/
