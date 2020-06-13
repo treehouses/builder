d8cb235b5daec648182bd8a2ca441342cf6b1fb5
ComputerOnFire <computeronfire@protonmail.com>
2020-06-12 20:49:41 -0500

    remove echo and pwd

--- scripts.d/20_tiv.sh
+++ scripts.d/20_tiv.sh
@@ -2,6 +2,4 @@
 
 git clone https://github.com/stefanhaustein/TerminalImageViewer.git
 CXX=arm-linux-gnueabihf-g++ make -C TerminalImageViewer/src/main/cpp
-ls -al #where is tiv
-echo "$PWD"
 mv TerminalImageViewer/src/main/cpp/tiv "$PWD/mnt/img_root/usr/local/bin/"
