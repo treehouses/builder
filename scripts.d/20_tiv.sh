d290dd8b511d2566136a2dc61523bf50396cf1e8
ComputerOnFire <computeronfire@protonmail.com>
2020-06-18 01:40:01 -0500

    back to apt for docker-compose

---
+++ scripts.d/20_tiv.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+git clone https://github.com/stefanhaustein/TerminalImageViewer.git
+CXX=arm-linux-gnueabihf-g++ make -C TerminalImageViewer/src/main/cpp
+mv TerminalImageViewer/src/main/cpp/tiv  mnt/img_root/usr/local/bin/
