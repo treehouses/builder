828c01dc4271d8a86aad35c557a41364508176bf
John <3631329+ComputerOnFire@users.noreply.github.com>
2020-06-18 01:45:07 -0500

    Update 20_tiv.sh

--- scripts.d/20_tiv.sh
+++ scripts.d/20_tiv.sh
@@ -2,4 +2,4 @@
 
 git clone https://github.com/stefanhaustein/TerminalImageViewer.git
 CXX=arm-linux-gnueabihf-g++ make -C TerminalImageViewer/src/main/cpp
-mv TerminalImageViewer/src/main/cpp/tiv  mnt/img_root/usr/local/bin/
+mv TerminalImageViewer/src/main/cpp/tiv mnt/img_root/usr/local/bin/
