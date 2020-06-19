37471c1814c7a1abb50a7c391b79c3388dac6a38
ComputerOnFire <computeronfire@protonmail.com>
2020-06-19 18:59:33 -0500

    _apt install gh arm64

--- scripts.d/20_gh.sh
+++ scripts.d/20_gh.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-git clone https://github.com/cli/cli.git gh-cli
-env GOOS=linux GOARCH=arm GOOARM=6 make -C gh-cli
-mv gh-cli/bin/gh mnt/img_root/usr/local/bin/
+source lib.sh
+# git clone https://github.com/cli/cli.git gh-cli
+# env GOOS=linux GOARCH=arm GOOARM=6 make -C gh-cli
+# mv gh-cli/bin/gh mnt/img_root/usr/local/bin/
+
+wget -N https://github.com/cli/cli/releases/download/v0.10.0/gh_0.10.0_linux_amd64.deb
+_apt install gh_0.10.0_linux_amd64.deb
