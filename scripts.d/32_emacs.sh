a7aab2b063db2dead38f1d7e53178f671033e810
darthnoward <hhl055959@gmail.com>
2020-06-20 13:37:44 +0800

    fix code style

--- scripts.d/32_emacs.sh
+++ scripts.d/32_emacs.sh
@@ -4,7 +4,7 @@ source lib.sh
 
 wget https://ftp.gnu.org/pub/gnu/emacs/emacs-26.1.tar.gz
 tar -zxvf emacs-26.1.tar.gz
-cd emacs-26.1
+cd emacs-26.1 || exit
 ./configure --without-x --prefix=/usr/local/emacs --with-gnutls=no
 make
 _chroot make install
