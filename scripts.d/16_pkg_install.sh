53fc01d720441f0de5e42afa1a586ba00b8b65c7
darthnoward <51189233+darthnoward@users.noreply.github.com>
2020-06-20 11:03:42 +0800

    test

--- scripts.d/16_pkg_install.sh
+++ scripts.d/16_pkg_install.sh
@@ -34,7 +34,7 @@ INSTALL_PACKAGES=(
     pagekite # tunnels command
     sl
     mc ranger
-    emacs # popular text editor 
+    emacs # a popular text editor 
     bats # unit testing
     libhdf5-dev libatlas-base-dev libjasper1 libqt4-test # opencv
 )
