33eef04d9542603e21e3707c7e467ed4a08d5b45
darthnoward <hhl055959@gmail.com>
2020-06-20 15:57:13 +0800

    mental breakdown

--- scripts.d/32_emacs.sh
+++ scripts.d/32_emacs.sh
@@ -5,7 +5,7 @@ source lib.sh
 wget https://ftp.gnu.org/pub/gnu/emacs/emacs-26.1.tar.gz
 tar -zxvf emacs-26.1.tar.gz
 cd emacs-26.1 || exit
-./configure --without-x --prefix=/usr/local/emacs --with-gnutls=no
+./configure --without-x --with-gnutls=no
 make --quiet
 _op _chroot make install --quiet
-cd ../ && rm -rf emacs-26.1
+cd ../ && rm -rf emacs-26.1 && rm -rf emacs-26.1.tar.gz
