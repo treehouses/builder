06dcc737e7b8e1c67d46cff23c0f202e068c00c2
ComputerOnFire <computeronfire@protonmail.com>
2020-06-19 22:29:37 -0500

    new balena install

--- scripts.d/20_balena.sh
+++ scripts.d/20_balena.sh
@@ -1,44 +1,45 @@
 #!/bin/bash
 
-exit 0
-
 source lib.sh
 
 echo "Balena installation"
 
-# get the latest version
-releases=$(curl -s https://api.github.com/repos/balena-os/balena-engine/releases/latest -H "Authorization: token $GITHUB_KEY" | jq -r ".assets[].browser_download_url")
-armv6link=$(echo "$releases" | tr " " "\\n" | grep armv6)
-armv7link=$(echo "$releases" | tr " " "\\n" | grep armv7)
+_op _chroot curl -sfL https://balena.io/engine/install.sh | sh
+_op _chroot rm install.sh
 
-# armv7
-wget -c "$armv7link"
-tar xvzf "$(basename "$armv7link")" ./balena-engine/balena-engine
-mv balena-engine/balena-engine mnt/img_root/usr/bin/balena-engine-armv7l
-_op _chroot chown root:root /usr/bin/balena-engine-armv7l
-rm -rf balena-engine/
+# # get the latest version
+# releases=$(curl -s https://api.github.com/repos/balena-os/balena-engine/releases/latest -H "Authorization: token $GITHUB_KEY" | jq -r ".assets[].browser_download_url")
+# armv6link=$(echo "$releases" | tr " " "\\n" | grep armv6)
+# armv7link=$(echo "$releases" | tr " " "\\n" | grep armv7)
 
-# armv6
-wget -c "$armv6link"
-tar xvzf "$(basename "$armv6link")" ./balena-engine/balena-engine
-mv balena-engine/balena-engine mnt/img_root/usr/bin/balena-engine-armv6l
-_op _chroot chown root:root /usr/bin/balena-engine-armv6l
-rm -rf balena-engine/
+# # armv7
+# wget -c "$armv7link"
+# tar xvzf "$(basename "$armv7link")" ./balena-engine/balena-engine
+# mv balena-engine/balena-engine mnt/img_root/usr/bin/balena-engine-armv7l
+# _op _chroot chown root:root /usr/bin/balena-engine-armv7l
+# rm -rf balena-engine/
 
-_op _chroot touch /usr/bin/balena-engine
-_op _chroot ln -sr /usr/bin/balena-engine /usr/bin/balena-engine-containerd
-_op _chroot ln -sr /usr/bin/balena-engine /usr/bin/balena-engine-containerd-ctr
-_op _chroot ln -sr /usr/bin/balena-engine /usr/bin/balena-engine-containerd-shim
-_op _chroot ln -sr /usr/bin/balena-engine /usr/bin/balena-engine-daemon
-_op _chroot ln -sr /usr/bin/balena-engine /usr/bin/balena-engine-proxy
-_op _chroot ln -sr /usr/bin/balena-engine /usr/bin/balena-engine-runc
-_op _chroot ln -sr /usr/bin/balena-engine /usr/bin/balena
-_op _chroot rm /usr/bin/balena-engine
-_op _chroot ln -sr /usr/bin/balena-engine-armv7l /usr/bin/balena-engine
+# # armv6
+# wget -c "$armv6link"
+# tar xvzf "$(basename "$armv6link")" ./balena-engine/balena-engine
+# mv balena-engine/balena-engine mnt/img_root/usr/bin/balena-engine-armv6l
+# _op _chroot chown root:root /usr/bin/balena-engine-armv6l
+# rm -rf balena-engine/
 
-_op _chroot groupadd balena
-_op _chroot usermod -aG balena pi
-_op _chroot usermod -aG balena root
+# _op _chroot touch /usr/bin/balena-engine
+# _op _chroot ln -sr /usr/bin/balena-engine /usr/bin/balena-engine-containerd
+# _op _chroot ln -sr /usr/bin/balena-engine /usr/bin/balena-engine-containerd-ctr
+# _op _chroot ln -sr /usr/bin/balena-engine /usr/bin/balena-engine-containerd-shim
+# _op _chroot ln -sr /usr/bin/balena-engine /usr/bin/balena-engine-daemon
+# _op _chroot ln -sr /usr/bin/balena-engine /usr/bin/balena-engine-proxy
+# _op _chroot ln -sr /usr/bin/balena-engine /usr/bin/balena-engine-runc
+# _op _chroot ln -sr /usr/bin/balena-engine /usr/bin/balena
+# _op _chroot rm /usr/bin/balena-engine
+# _op _chroot ln -sr /usr/bin/balena-engine-armv7l /usr/bin/balena-engine
 
-_op _chroot rm -rf /var/lib/balena-engine
-_op _chroot ln -sr /var/lib/docker /var/lib/balena-engine
+# _op _chroot groupadd balena
+# _op _chroot usermod -aG balena pi
+# _op _chroot usermod -aG balena root
+
+# _op _chroot rm -rf /var/lib/balena-engine
+# _op _chroot ln -sr /var/lib/docker /var/lib/balena-engine
