5abd132d1e612b56a8155d922144c289f29693fb
ComputerOnFire <computeronfire@protonmail.com>
2020-06-19 20:23:18 -0500

    find directory

--- scripts.d/20_gh.sh
+++ scripts.d/20_gh.sh
@@ -6,5 +6,5 @@ source lib.sh
 # mv gh-cli/bin/gh mnt/img_root/usr/local/bin/
 
 wget -N https://github.com/cli/cli/releases/download/v0.10.0/gh_0.10.0_linux_arm64.deb -P mnt/img_root/
-_op _chroot pwd
+_op _chroot ls -al
 _apt install gh_0.10.0_linux_arm64.deb
