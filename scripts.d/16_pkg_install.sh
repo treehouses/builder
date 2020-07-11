0ad6e696f8f13e39398d04112482dbdcb145a0a2
ComputerOnFire <computeronfire@protonmail.com>
2020-07-10 19:38:55 -0500

    python3 deps

--- scripts.d/16_pkg_install.sh
+++ scripts.d/16_pkg_install.sh
@@ -37,7 +37,7 @@ INSTALL_PACKAGES=(
     bats # unit testing
     libhdf5-dev libatlas-base-dev libjasper1 libqt4-test # opencv
     imagemagick # tiv
-    python-bcrypt python-nacl # fix slow pip
+    python3-bcrypt python3-nacl # fix slow pip
 )
 
 if [[ ${INSTALL_PACKAGES:-} ]] ; then
