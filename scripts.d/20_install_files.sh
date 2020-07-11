d44362630fb7da41c5d2fe0e1a77b56a8e6fb6ae
ComputerOnFire <computeronfire@protonmail.com>
2020-07-11 02:14:50 -0500

    exit to fix

--- scripts.d/20_install_files.sh
+++ scripts.d/20_install_files.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+exit 0
+
 source lib.sh
 echo "Installing files"
 (cd install || die "ERROR: install folder doesn't exist, exiting"; tar c .) | _op _chroot tar vx --owner=root --group=root
--- scripts.d/50_rm_files.sh
+++ scripts.d/50_rm_files.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+exit 0
+
 source lib.sh
 # List of files/directories to remove
 REMOVE=(
