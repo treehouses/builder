90f6d5a9804a41fe2a17e89b9a60a851a2517e59
ComputerOnFire <computeronfire@protonmail.com>
2020-07-27 20:09:42 -0500

    split scripts

---
+++ scripts.d/20_install_balena_socket.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+cat << EOF > /etc/systemd/system/balena.socket
+[Unit]
+Description=Balena Socket for the API
+PartOf=balena.service
+
+[Socket]
+ListenStream=/var/run/balena.sock
+SocketMode=0660
+SocketUser=root
+SocketGroup=balena
+
+[Install]
+WantedBy=sockets.target
+EOF
