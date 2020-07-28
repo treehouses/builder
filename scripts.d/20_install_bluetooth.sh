2cffb6d93cf518fbaed7d6d731c77ac78e9c0be8
ComputerOnFire <computeronfire@protonmail.com>
2020-07-27 20:09:42 -0500

    split scripts

---
+++ scripts.d/20_install_bluetooth.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+cat << EOF > /etc/systemd/system/rpibluetooth.service
+[Unit]
+Description=Bluetooth server
+After=rpibluetooth.service
+Requires=rpibluetooth.service bluetooth.service
+
+StartLimitIntervalSec=500
+StartLimitBurst=5
+
+[Service]
+Restart=always
+RestartSec=5s
+
+ExecStart=/usr/bin/python3 /usr/local/bin/bluetooth-server.py &
+
+[Install]
+WantedBy=multi-user.target
+EOF
