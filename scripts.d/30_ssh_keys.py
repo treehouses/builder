bc018d63a7f165c9c3dcaf663433febdb938cfe7
ComputerOnFire <computeronfire@protonmail.com>
2020-07-30 01:18:43 -0500

    ssh keys disabled

--- scripts.d/30_ssh_keys.py
+++ scripts.d/30_ssh_keys.py
@@ -1,43 +1,42 @@
-#!/usr/bin/env python3
-exit 0 
-# Obtain the public ssh keys of each of the public members of open-learning-exchange organization
+# #!/usr/bin/env python3
+# # Obtain the public ssh keys of each of the public members of open-learning-exchange organization
 
-import requests
-import os
+# import requests
+# import os
 
-headers = {
-    "Authorization": "token %s" % os.environ.get("GITHUB_KEY"),
-    "Connection": "close"
-}
+# headers = {
+#     "Authorization": "token %s" % os.environ.get("GITHUB_KEY"),
+#     "Connection": "close"
+# }
 
-# Retrieve a list of public members using gitHub API
+# # Retrieve a list of public members using gitHub API
 
-def get_members(members,page):
-    api = 'https://api.github.com/teams/3087744/members?per_page=100&page=' + str(page)
+# def get_members(members,page):
+#     api = 'https://api.github.com/teams/3087744/members?per_page=100&page=' + str(page)
 
-    request = requests.get(api, headers=headers)
-    users = request.json()
-    for user in users:
-        members.append(user['login'])
-    count = len(users)
+#     request = requests.get(api, headers=headers)
+#     users = request.json()
+#     for user in users:
+#         members.append(user['login'])
+#     count = len(users)
 
-    if(count > 0):
-        return get_members(members,page + 1)
-    else:
-        return members
+#     if(count > 0):
+#         return get_members(members,page + 1)
+#     else:
+#         return members
 
-members = get_members([],1)
-print ("Found %d members. Getting keys for users" % len(members))
+# members = get_members([],1)
+# print ("Found %d members. Getting keys for users" % len(members))
 
-f = open("authorized_keys", "w")
-for member in members:
-    keys_content = requests.get('https://github.com/' + member + '.keys', headers=headers).content
-    keys = keys_content.splitlines()
-    if len(keys):
-        print ("info: user %s has keys" % member)
-    else:
-        print ("info: user %s has NO keys" % member)
+# f = open("authorized_keys", "w")
+# for member in members:
+#     keys_content = requests.get('https://github.com/' + member + '.keys', headers=headers).content
+#     keys = keys_content.splitlines()
+#     if len(keys):
+#         print ("info: user %s has keys" % member)
+#     else:
+#         print ("info: user %s has NO keys" % member)
 
-    for key in keys:
-        f.write("%s %s\n" % (key.strip().decode("utf-8"), member))
-f.close()
+#     for key in keys:
+#         f.write("%s %s\n" % (key.strip().decode("utf-8"), member))
+# f.close()
