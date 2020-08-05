8b53da4750653cfb1ad750ce00098648f6d599ad
ComputerOnFire <computeronfire@protonmail.com>
2020-08-05 08:07:13 -0500

    ignore on arm64

--- scripts.d/05_mirrors.sh
+++ scripts.d/05_mirrors.sh
@@ -2,6 +2,10 @@
 
 source lib.sh
 
+if uname -m | grep -q "aarch64" ; then
+    exit 0
+fi
+
 cat << EOF > mnt/img_root/etc/apt/sources.list
 deb http://mirrors.ocf.berkeley.edu/raspbian/raspbian buster main contrib non-free rpi
 #deb http://raspbian.raspberrypi.org/raspbian/ buster main contrib non-free rpi
