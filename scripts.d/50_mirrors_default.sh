201ecc4dd9ac2f6a40a011a4c7d65be2a6a7f836
ComputerOnFire <computeronfire@protonmail.com>
2020-08-05 20:40:05 -0500

    mirrordirector url fix

--- scripts.d/50_mirrors_default.sh
+++ scripts.d/50_mirrors_default.sh
@@ -8,7 +8,7 @@ fi
 
 cat << EOF > mnt/img_root/etc/apt/sources.list
 deb http://raspbian.raspberrypi.org/raspbian/ buster main contrib non-free rpi
-#deb http://mirrordirector.raspbian.org/ buster main contrib non-free rpi
+#deb http://mirrordirector.raspbian.org/raspbian/ buster main contrib non-free rpi
 # Uncomment line below then 'apt-get update' to enable 'apt-get source'
 #deb-src http://raspbian.raspberrypi.org/raspbian/ buster main contrib non-free rpi
 EOF
