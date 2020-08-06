f6c354c5c664acda32efeda9201940e13b74201b
ComputerOnFire <computeronfire@protonmail.com>
2020-08-05 20:24:06 -0500

    mirrordirector

--- scripts.d/05_mirrors.sh
+++ scripts.d/05_mirrors.sh
@@ -7,7 +7,7 @@ if uname -m | grep -q "aarch64" ; then
 fi
 
 cat << EOF > mnt/img_root/etc/apt/sources.list
-deb http://mirrors.ocf.berkeley.edu/raspbian/raspbian buster main contrib non-free rpi
+deb http://mirrordirector.raspbian.org/ buster main contrib non-free rpi
 #deb http://raspbian.raspberrypi.org/raspbian/ buster main contrib non-free rpi
 # Uncomment line below then 'apt-get update' to enable 'apt-get source'
 #deb-src http://raspbian.raspberrypi.org/raspbian/ buster main contrib non-free rpi
