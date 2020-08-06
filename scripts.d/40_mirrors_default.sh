683e5a7a677b17a8deff6d5b71d1859a49f36099
John <3631329+ComputerOnFire@users.noreply.github.com>
2020-08-06 02:02:01 -0500

    Update 40_mirrors_default.sh

--- scripts.d/40_mirrors_default.sh
+++ scripts.d/40_mirrors_default.sh
@@ -2,10 +2,6 @@
 
 source lib.sh
 
-if uname -m | grep -q "aarch64" ; then
-    exit 0
-fi
-
 cat << EOF > mnt/img_root/etc/apt/sources.list
 deb http://raspbian.raspberrypi.org/raspbian/ buster main contrib non-free rpi
 #deb http://mirrors.ocf.berkeley.edu/raspbian/raspbian buster main contrib non-free rpi
