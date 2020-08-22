22713ff4a9d315b38b2e836b2ee0dfd70ab5238d
darthnoward <51189233+darthnoward@users.noreply.github.com>
2020-08-22 10:44:47 +0800

    Update 20_telegram_cli.sh

--- scripts.d/20_telegram_cli.sh
+++ scripts.d/20_telegram_cli.sh
@@ -10,8 +10,7 @@ sed -i "s/\-fPIC//" Makefile.in
 sed -i "s/\-Werror //" Makefile.in
 ./configure
 CXX=arm-linux-gnueabihf-g++ make
-cp ./bin/telegram-cli /mnt/img_root/usr/local/bin/telegram-cli
-mkdir -p /mnt/img_root/etc/telegram-cli
-cp ./server.pub /mnt/img_root/etc/telegram-cli/server.pub
-cd ../
-rm -rf tg
+cd .. || exit 1
+cp tg/bin/telegram-cli mnt/img_root/usr/local/bin/telegram-cli
+mkdir -p mnt/img_root/etc/telegram-cli
+cp tg/server.pub mnt/img_root/etc/telegram-cli/server.pub
