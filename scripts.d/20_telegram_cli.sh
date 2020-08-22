6ed09addb630773e8af3cb65d3e87f2f284bec65
darthnoward <hhl055959@gmail.com>
2020-08-22 10:01:20 +0800

    fix for compiling on travis

--- scripts.d/20_telegram_cli.sh
+++ scripts.d/20_telegram_cli.sh
@@ -1,11 +1,15 @@
 #! /bin/bash
 
 git clone https://github.com/vysheng/tg --recursive
-cd tg
-sed -i '116d' ./tgl/mtproto-utils.c
-sed -i '105d' ./tgl/mtproto-utils.c
+
+cd tg || exit 1
+sed -i '107d' ./tgl/mtproto-utils.c
+sed -i '101d' ./tgl/mtproto-utils.c
+sed -i "s/\-rdynamic //" Makefile.in
+sed -i "s/\-fPIC//" Makefile.in
+sed -i "s/\-Werror //" Makefile.in
 ./configure
-make
+CXX=arm-linux-gnueabihf-g++ make
 cp ./bin/telegram-cli /mnt/img_root/usr/local/bin/telegram-cli
 mkdir -p /mnt/img_root/etc/telegram-cli
 cp ./server.pub /mnt/img_root/etc/telegram-cli/server.pub
