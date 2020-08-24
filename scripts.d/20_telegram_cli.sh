d7bc8363b9b8f91beb89755e8507a0660c8edfeb
darthnoward <hhl055959@gmail.com>
2020-08-24 23:44:56 +0800

    update release url

--- scripts.d/20_telegram_cli.sh
+++ scripts.d/20_telegram_cli.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-wget -P mnt/img_root/usr/local/bin https://github.com/darthnoward/tg/releases/download/1.3.1/telegram-cli || exit 1
+wget -P mnt/img_root/usr/local/bin https://github.com/darthnoward/tg/releases/download/1.4.1/telegram-cli || exit 1
 chmod +x mnt/img_root/usr/local/bin/telegram-cli
 mkdir -p mnt/img_root/etc/telegram-cli
 wget -P mnt/img_root/etc/telegram-cli https://github.com/vysheng/tg/raw/master/tg-server.pub || exit 1
