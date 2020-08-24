0a93ad3bcc5b4762d40421964ac354909fb50f7b
darthnoward <hhl055959@gmail.com>
2020-08-24 18:45:58 +0800

    add telegram-cli

--- scripts.d/16_pkg_install.sh
+++ scripts.d/16_pkg_install.sh
@@ -28,6 +28,7 @@ INSTALL_PACKAGES=(
     htop
     speedtest-cli # speedtest.net
     libffi-dev # for building docker-compose using pip
+    libconfig9 # for running telegram-cli
     python3-coral-enviro # Coral environmental board
     bc # for memory command
     libusb-dev # for usb.sh
---
+++ scripts.d/20_telegram_cli.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+wget -P mnt/img_root/usr/local/bin https://github.com/darthnoward/tg/releases/download/1.3.1/telegram-cli || exit 1
+mkdir -p mnt/img_root/etc/telegram-cli
+wget -P mnt/img_root/etc/telegram-cli https://github.com/vysheng/tg/raw/master/tg-server.pub || exit 1
