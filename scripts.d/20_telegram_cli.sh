2e712e9e95390c79a2f7caa15f7a51da55357723
darthnoward <hhl055959@gmail.com>
2020-08-27 10:23:28 +0800

    change source to kenorb-contrib

--- scripts.d/20_telegram_cli.sh
+++ scripts.d/20_telegram_cli.sh
@@ -1,13 +1,8 @@
 #! /bin/bash
 
-git clone https://github.com/vysheng/tg --recursive
+git clone https://github.com/kenorb-contrib/tg --recursive
 
 cd tg || exit 1
-sed -i '107d' ./tgl/mtproto-utils.c
-sed -i '101d' ./tgl/mtproto-utils.c
-sed -i "s/\-rdynamic //" Makefile.in
-sed -i "s/\-fPIC//" Makefile.in
-sed -i "s/\-Werror //" Makefile.in
 ./configure
 CXX=arm-linux-gnueabihf-g++ make
 cd .. || exit 1
