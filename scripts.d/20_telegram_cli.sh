f081612ec032cfd8914c1a4ca576d432b5224c5c
darthnoward <51189233+darthnoward@users.noreply.github.com>
2020-08-27 10:24:43 +0800

    Update 20_telegram_cli.sh

--- scripts.d/20_telegram_cli.sh
+++ scripts.d/20_telegram_cli.sh
@@ -4,7 +4,7 @@ git clone https://github.com/kenorb-contrib/tg --recursive
 
 cd tg || exit 1
 ./configure
-CXX=arm-linux-gnueabihf-g++ make
+CXX=arm-linux-gnueabihf-gcc make
 cd .. || exit 1
 cp tg/bin/telegram-cli mnt/img_root/usr/local/bin/telegram-cli
 mkdir -p mnt/img_root/etc/telegram-cli
