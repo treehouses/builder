cec1b10dcf048c7d8ae39d3830292e1ca5c32fc1
jiaxuan <khongjiaxuan@gmail.com>
2020-09-24 14:35:38 +0800

    update terraform to newest arm edition

--- scripts.d/32_terraform.sh
+++ scripts.d/32_terraform.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-wget https://releases.hashicorp.com/terraform/0.12.24/terraform_0.12.24_linux_arm.zip
-unzip terraform_0.12.24_linux_arm.zip
+wget https://releases.hashicorp.com/terraform/0.13.3/terraform_0.13.3_linux_arm.zip
+unzip terraform_0.13.3_linux_arm.zip
 
 mkdir -p mnt/img_root/usr/local/bin/
 mv terraform mnt/img_root/usr/local/bin/.
