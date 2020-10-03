f633937a022feccdad0d807f3ca5fc63a6cf7021
koxuan <4152386+koxuan@users.noreply.github.com>
2020-10-03 12:33:47 +0800

    add log2ram

---
+++ scripts.d/36_log2ram.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+_op _chroot curl -Lo log2ram.tar.gz https://github.com/azlux/log2ram/archive/master.tar.gz
+_op _chroot tar xf log2ram.tar.gz
+_op _chroot rm log2ram.tar.gz
+_op _chroot cd log2ram-master 
+_op _chroot ./install.sh
+_op _chroot cd ..
+_op _chroot rm -r log2ram-master
+_op _chroot systemctl stop log2ram
+_op _chroot systemctl disable log2ram
+
