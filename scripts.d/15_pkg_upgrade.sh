57bfdf06a2bff86eec136fe2a35fcab2bea2e999
rjpadilla <rjpadilla.dev@outlook.com>
2021-04-26 15:26:03 -0400

    exit after update

--- scripts.d/15_pkg_upgrade.sh
+++ scripts.d/15_pkg_upgrade.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 source lib.sh
+_apt update || die "Could not update package sources"
+_apt dist-upgrade || die "Could not upgrade system"
+exit 0
 
 echo "Holding nodejs Package"
 _op _chroot apt-mark hold nodejs
