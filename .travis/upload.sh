ce2f8391a976dea0818e8a0f343a86ff2b4a6bf8
rjpadilla <23529006+rjpadilla@users.noreply.github.com>
2021-04-30 18:15:51 -0400

    cating out id_deploy

--- .travis/upload.sh
+++ .travis/upload.sh
@@ -5,6 +5,7 @@ source .travis/utils.sh
 echo "Uploading image to dev"
 echo "echoing out $image_gz"
 echo "echoing out $image_sha1"
+cat .travis/id_deploy 
 rsync -P -e .travis/ssh.sh "$image_gz" "$image_sha1" deploy@download.ole.org:/data/images/.rj
 
 if release_is_number; then
