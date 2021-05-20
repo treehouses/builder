1c4310e1363dfd160c4f5ae6473438e308a76ca4
rjpadilla <23529006+rjpadilla@users.noreply.github.com>
2021-05-19 20:33:28 -0400

    uncommented coral

--- scripts.d/05_add_keys.sh
+++ scripts.d/05_add_keys.sh
@@ -6,7 +6,7 @@ source lib.sh
 ADD_REPO_KEYS=(
     68576280 # nodesource
     0EBFCD88 # Docker Release (CE deb)
-    # 6A030B21BA07F4FB # Coral
+    6A030B21BA07F4FB # Coral
 )
 
 GPG="gpg --no-permission-warning --no-default-keyring --quiet --keyring "
