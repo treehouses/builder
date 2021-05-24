433f39ac8b628b16757fc4a0bff3ca06ec690754
dogi <dogi@users.noreply.github.com>
2021-05-24 15:41:17 -0400

    Update 20_tiv.sh

--- scripts.d/20_tiv.sh
+++ scripts.d/20_tiv.sh
@@ -2,7 +2,7 @@
 
 git clone https://github.com/stefanhaustein/TerminalImageViewer.git
 (
-  cd TerminalImageViewer
+  cd TerminalImageViewer || exit 0
   git checkout e5ef996c4432054c05e5af6965d405933c8391d1
 )
 CXX=arm-linux-gnueabihf-g++ make -C TerminalImageViewer/src/main/cpp
