16808d271f30723692626adc17067ad5c801436c
rjpadilla <23529006+rjpadilla@users.noreply.github.com>
2021-05-26 18:02:36 -0400

    update 20_tiv.sh (fixes #891)

--- scripts.d/20_tiv.sh
+++ scripts.d/20_tiv.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
 git clone https://github.com/stefanhaustein/TerminalImageViewer.git
-(
-  cd TerminalImageViewer || exit 0
-  git checkout e5ef996c4432054c05e5af6965d405933c8391d1
-)
 CXX=arm-linux-gnueabihf-g++ make -C TerminalImageViewer/src/main/cpp
 mv TerminalImageViewer/src/main/cpp/tiv mnt/img_root/usr/local/bin/
