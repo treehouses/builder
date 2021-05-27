31121a24ca25c7e3814b8d79d22b0eac3549229b
rjpadilla <23529006+rjpadilla@users.noreply.github.com>
2021-05-26 23:22:18 -0400

    update mirrors

--- scripts.d/05_better_mirrors.sh
+++ scripts.d/05_better_mirrors.sh
@@ -5,7 +5,7 @@ source lib.sh
 cp mnt/img_root/etc/apt/sources.list mnt/img_root/etc/apt/sources.list.orig
 
 cat << EOF > mnt/img_root/etc/apt/sources.list
-#deb http://raspbian.raspberrypi.org/raspbian/ buster main contrib non-free rpi
+deb http://raspbian.raspberrypi.org/raspbian/ buster main contrib non-free rpi
 #deb http://mirrors.ocf.berkeley.edu/raspbian/raspbian buster main contrib non-free rpi
 # Uncomment line below then 'apt-get update' to enable 'apt-get source'
 #deb-src http://raspbian.raspberrypi.org/raspbian/ buster main contrib non-free rpi
