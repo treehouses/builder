8550e905e89af55614b7724b25c3784e37b7c229
dogi <stefan@unterhauser.name>
2021-06-03 16:50:53 -0400

    remove old script

--- scripts.d/20_install_files.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-exit 0
-
-source lib.sh
-echo "Installing files"
-#(cd install || die "ERROR: install folder doesn't exist, exiting"; tar c .) | _op _chroot tar vx --owner=root --group=root
