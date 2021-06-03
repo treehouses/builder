f69f31d27ed0c74b7da1c73674101e7b7e91495b
rjpadilla <23529006+rjpadilla@users.noreply.github.com>
2021-06-02 21:17:46 -0400

    case statements for mirrors

--- scripts.d/05_better_mirrors.sh
+++ scripts.d/05_better_mirrors.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
+architecture="$1"
 source lib.sh
 
 cp mnt/img_root/etc/apt/sources.list mnt/img_root/etc/apt/sources.list.orig
-
-cat << EOF > mnt/img_root/etc/apt/sources.list
+case "$architecture"  in
+  "arm" | "")
+    cat << EOF > mnt/img_root/etc/apt/sources.list
 #deb http://raspbian.raspberrypi.org/raspbian/ buster main contrib non-free rpi
 deb http://mirrors.ocf.berkeley.edu/raspbian/raspbian buster main contrib non-free rpi
 # Uncomment line below then 'apt-get update' to enable 'apt-get source'
 #deb-src http://raspbian.raspberrypi.org/raspbian/ buster main contrib non-free rpi
 EOF
+  ;;
+  "arm64")
+    #Todo http://debian-archive.trafficmanager.net/debian/
+  ;;
+esac
