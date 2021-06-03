f09c8d03de756d5ec2cd6451a250578d87391a0c
rjpadilla <23529006+rjpadilla@users.noreply.github.com>
2021-06-02 22:23:54 -0400

    brought back lib packages

--- scripts.d/16_pkg_install.sh
+++ scripts.d/16_pkg_install.sh
@@ -38,7 +38,7 @@ INSTALL_PACKAGES=(
     gh
     mc ranger
     bats # unit testing
-    # libhdf5-dev libatlas-base-dev libjasper1 libqt4-test # opencv
+    libhdf5-dev libatlas-base-dev libqt4-test # opencv # libjasper1 
     imagemagick # tiv
     python3-bcrypt python3-nacl # fix slow pip
 )
