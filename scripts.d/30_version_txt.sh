4b767971b9f12388bb7e8b19b7bf6ce36f5013e2
rjpadilla <23529006+rjpadilla@users.noreply.github.com>
2021-06-03 01:00:39 -0400

    changed arm and added netcat

--- scripts.d/30_version_txt.sh
+++ scripts.d/30_version_txt.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
-
+architecture="$1"
 set -e
 
 ROOT=mnt/img_root
-version=$(git tag --sort=-creatordate | sed -n '1p')
+case "$architecture" in
+  "armhf" | "") 
+    version=$(git tag --sort=-creatordate | sed -n '1p')
+  ;;
+  "arm64") 
+    version="$(git tag --sort=-creatordate | sed -n '1p')-$architecture"
+  ;;
+esac
 echo "writing version.txt: $version"
 echo "$version" > $ROOT/boot/version.txt
