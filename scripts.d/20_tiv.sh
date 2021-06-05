74355d6649c6323b0c1f63b6c6a6387759f53ca4
rjpadilla <23529006+rjpadilla@users.noreply.github.com>
2021-06-04 21:50:38 -0400

    added repo reference (fixes #897)

--- scripts.d/20_tiv.sh
+++ scripts.d/20_tiv.sh
@@ -5,7 +5,7 @@ git clone https://github.com/stefanhaustein/TerminalImageViewer.git
   cd TerminalImageViewer || exit 0
   workingver=e69c53e4cc03f370fd335b65ccdeb97d3ece294c
   currentver=$(git rev-parse HEAD)
-  [[ "$workingver" != "$currentver" ]] && echo "A new version has been released, test and update the latest release."
+  [[ "$workingver" != "$currentver" ]] && echo "A new version has been released, test and update to the latest release at 'https://github.com/stefanhaustein/TerminalImageViewer'."
   git checkout $workingver
 )
 CXX=arm-linux-gnueabihf-g++ make -C TerminalImageViewer/src/main/cpp
