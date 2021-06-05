a67af5ded21be3d387d8d2f17169c6a11ab869f9
rjpadilla <23529006+rjpadilla@users.noreply.github.com>
2021-06-04 23:01:15 -0400

    added arm64 compiler

--- scripts.d/20_tiv.sh
+++ scripts.d/20_tiv.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+architecture="$1"
+
 git clone https://github.com/stefanhaustein/TerminalImageViewer.git
 (
   cd TerminalImageViewer || exit 0
@@ -8,5 +10,13 @@ git clone https://github.com/stefanhaustein/TerminalImageViewer.git
   [[ "$workingver" != "$currentver" ]] && echo "TODO: a new version has been released, check https://github.com/stefanhaustein/TerminalImageViewer"
   git checkout $workingver
 )
-CXX=arm-linux-gnueabihf-g++ make -C TerminalImageViewer/src/main/cpp
+
+case "$architecture" in
+    "armhf" | "")
+      CXX=arm-linux-gnueabihf-g++ make -C TerminalImageViewer/src/main/cpp
+    ;;
+    "arm64")
+      CXX=aarch64-linux-gnu-g++ make -C TerminalImageViewer/src/main/cpp
+    ;;
+esac
 mv TerminalImageViewer/src/main/cpp/tiv mnt/img_root/usr/local/bin/
