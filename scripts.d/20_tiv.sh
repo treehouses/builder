39a634c15535caf98ac5046a335768e33aab9b68
rjpadilla <23529006+rjpadilla@users.noreply.github.com>
2021-06-04 21:24:36 -0400

    locking working tiv version (fixes #897)

--- scripts.d/20_tiv.sh
+++ scripts.d/20_tiv.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
 git clone https://github.com/stefanhaustein/TerminalImageViewer.git
+(
+  cd TerminalImageViewer || exit 0
+  workingver=e69c53e4cc03f370fd335b65ccdeb97d3ece294c
+  currentver=$(git rev-parse HEAD)
+  [[ "$workingver" != "$currentver" ]] && echo "A new version has been released, test and update the latest release."
+  git checkout $workingver
+)
 CXX=arm-linux-gnueabihf-g++ make -C TerminalImageViewer/src/main/cpp
 mv TerminalImageViewer/src/main/cpp/tiv mnt/img_root/usr/local/bin/
