c6326cf63ca4a4cc0f09c5dcce07c55f4a62804f
dogi <dogi@users.noreply.github.com>
2025-02-03 02:26:09 -0500

    Update 00_disable_autoresize.sh

--- scripts.d/00_disable_autoresize.sh
+++ scripts.d/00_disable_autoresize.sh
@@ -2,4 +2,6 @@
 
 source lib.sh
 
-sed -i 's| quiet init=/usr/lib/raspberrypi-sys-mods/firstboot||' mnt/img_root/boot/firmware/cmdline.txt || die "Could notdisable autoresize"
+echo cmdline.txt
+cat mnt/img_root/boot/cmdline.txt
+sed -i 's| quiet init=/usr/lib/raspberrypi-sys-mods/firstboot||' mnt/img_root/boot/cmdline.txt || die "Could notdisable autoresize"
