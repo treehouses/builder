0510d4941ee68891fd6af27ba34d985126d0c608
dogi <dogi@users.noreply.github.com>
2025-02-03 02:22:16 -0500

    Update 05_add_keys.sh

--- scripts.d/05_add_keys.sh
+++ scripts.d/05_add_keys.sh
@@ -4,10 +4,10 @@ source lib.sh
 
 # List of extra GPG keys to import from the LOCAL SYSTEM!
 ADD_REPO_KEYS=(
-    68576280 # nodesource
-    0EBFCD88 # docker release (ce deb)
-    C99B11DEB97541F0 # gh
-    8B57C5C2836F4BEB # coral
+    #68576280 # nodesource
+    #0EBFCD88 # docker release (ce deb)
+    #C99B11DEB97541F0 # gh
+    #8B57C5C2836F4BEB # coral
 )
 
 GPG="gpg --no-permission-warning --no-default-keyring --quiet --keyring "
