e3736f703e8833c8058f929970c6b3fd81d69a30
dogi <dogi@users.noreply.github.com>
2025-02-03 05:40:14 -0500

    Update 05_add_keys.sh

--- scripts.d/05_add_keys.sh
+++ scripts.d/05_add_keys.sh
@@ -6,7 +6,7 @@ source lib.sh
 ADD_REPO_KEYS=(
     68576280 # nodesource
     0EBFCD88 # docker release (ce deb)
-    C99B11DEB97541F0 # gh
+    #C99B11DEB97541F0 # gh
     #8B57C5C2836F4BEB # coral
 )
 
