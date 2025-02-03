34d9864b5f48857b9e97295706f8cb0d1b05fa6a
dogi <dogi@users.noreply.github.com>
2025-02-03 03:50:21 -0500

    Update 05_add_keys.sh

--- scripts.d/05_add_keys.sh
+++ scripts.d/05_add_keys.sh
@@ -5,7 +5,7 @@ source lib.sh
 # List of extra GPG keys to import from the LOCAL SYSTEM!
 ADD_REPO_KEYS=(
     #68576280 # nodesource
-    #0EBFCD88 # docker release (ce deb)
+    0EBFCD88 # docker release (ce deb)
     #C99B11DEB97541F0 # gh
     #8B57C5C2836F4BEB # coral
 )
