41408c05904c8a5d7ebbdf2ee6245c01aab8075b
dogi <dogi@users.noreply.github.com>
2025-02-05 11:27:23 -0500

    Update 05_add_repos.sh

--- scripts.d/05_add_repos.sh
+++ scripts.d/05_add_repos.sh
@@ -8,11 +8,11 @@ _apt update --allow-releaseinfo-change || die "Could not update package sources"
 _apt install "${pkgs[@]}"
 
 mkdir -p /etc/apt/trusted.gpg.d/
+#curl -fsSL https://deb.nodesource.com/gpgkey/nodesource.gpg.key | gpg --dearmor -o mnt/img_root/etc/apt/trusted.gpg.d/nodesource.gpg
 curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o mnt/img_root/etc/apt/trusted.gpg.d/docker.gpg
-curl -fsSL https://deb.nodesource.com/gpgkey/nodesource.gpg.key | gpg --dearmor -o mnt/img_root/etc/apt/trusted.gpg.d/nodesource.gpg
 
 ADD_REPOS=(
-    "deb [signed-by=/etc/apt/trusted.gpg.d/nodesource.gpg] https://deb.nodesource.com/node_20.x bookworm main"
+    #"deb [signed-by=/etc/apt/trusted.gpg.d/nodesource.gpg] https://deb.nodesource.com/node_20.x bookworm main"
     "deb [signed-by=/etc/apt/trusted.gpg.d/docker.gpg] https://download.docker.com/linux/debian bookworm stable"
     # curl https://cli.github.com/packages/githubcli-archive-keyring.gpg > keys/C99B11DEB97541F0.key
     #"deb [arch=aarch64] https://cli.github.com/packages stable main"
