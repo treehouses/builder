32d6a592eef21e874fb4896c996061020fee2401
dogi <dogi@users.noreply.github.com>
2025-02-05 05:14:44 -0500

    Update 20_tiv.sh

--- scripts.d/20_tiv.sh
+++ scripts.d/20_tiv.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-architecture="$1"
-
 git clone https://github.com/stefanhaustein/TerminalImageViewer.git
 (
   cd TerminalImageViewer || exit 0
@@ -11,12 +9,5 @@ git clone https://github.com/stefanhaustein/TerminalImageViewer.git
   git checkout $workingver
 )
 
-case "$architecture" in
-    "armhf" | "")
-      CXX=arm-linux-gnueabihf-g++ make -C TerminalImageViewer/src/main/cpp
-    ;;
-    "arm64")
-      CXX=aarch64-linux-gnu-g++ make -C TerminalImageViewer/src/main/cpp
-    ;;
-esac
+CXX=aarch64-linux-gnu-g++ make -C TerminalImageViewer/src/main/cpp
 mv TerminalImageViewer/src/main/cpp/tiv mnt/img_root/usr/local/bin/
