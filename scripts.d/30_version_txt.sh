cfc95ae9e17600a19ecaba9926a39f31b3e04fe6
dogi <dogi@users.noreply.github.com>
2025-02-05 09:01:19 -0500

    Update 30_version_txt.sh

--- scripts.d/30_version_txt.sh
+++ scripts.d/30_version_txt.sh
@@ -1,15 +1,8 @@
 #!/bin/sh
-architecture="$1"
 set -e
 
 ROOT=mnt/img_root
-case "$architecture" in
-  "armhf" | "") 
-    version=$(git tag --sort=-creatordate | sed -n '1p')
-  ;;
-  "arm64") 
-    version="$(git tag --sort=-creatordate | sed -n '1p')-$architecture"
-  ;;
-esac
+version="$(git tag --sort=-creatordate | sed -n '1p')-arm64"
+
 echo "writing version.txt: $version"
 echo "$version" > $ROOT/boot/version.txt
