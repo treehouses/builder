254f2c11e6da8d4a79aab6d471d096e84dc94424
dogi <dogi@users.noreply.github.com>
2025-02-05 13:07:25 -0500

    Update and rename 30_npminstall.sh to 31_npminstall.sh

--- scripts.d/31_npminstall.sh
+++ scripts.d/31_npminstall.sh
@@ -15,3 +15,6 @@ if [ ! -L "$PWD/mnt/img_root/etc/bash_completion.d/_treehouses" ]; then ln -sr "
 ls -al "$PWD/mnt/img_root/etc/bash_completion.d/_treehouses"
 npm config delete prefix
 rm -rf node_root
+
+_op _chroot treehouses sshkey github adduser dogi
+#_op _chroot treehouses sshkey github addteam treehouses support $APIKEY
