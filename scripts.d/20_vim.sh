be12c59b97c50ff964666aea8031d907b86386f6
dogi <dogi@users.noreply.github.com>
2025-02-05 06:02:12 -0500

    Update 20_vim.sh

--- scripts.d/20_vim.sh
+++ scripts.d/20_vim.sh
@@ -7,6 +7,7 @@ _op _chroot /usr/bin/update-alternatives --display editor
 _op _chroot ls -l /usr/bin/vim.basic
 
 
-_op _chroot update-alternatives --set editor /usr/bin/vim.basic
+#_op _chroot update-alternatives --set editor /usr/bin/vim.basic
+_op _chroot update-alternatives --set editor /usr/bin/vim.tiny
 
 _op _chroot /usr/bin/update-alternatives --display editor
