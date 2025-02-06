9b1070626e53683776e5975c3b0f6e54f59ba3b3
dogi <dogi@users.noreply.github.com>
2025-02-06 01:51:25 -0500

    Update 15_pkg_upgrade.sh

--- scripts.d/15_pkg_upgrade.sh
+++ scripts.d/15_pkg_upgrade.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-exit 0
+#exit 0
 source lib.sh
 
 # temporay fix to not break GUI icons and background
@@ -10,10 +10,10 @@ _op _chroot apt-mark hold libfm-gtk4
 _op _chroot apt-mark hold libfm-modules
 _op _chroot apt-mark hold libfm4
 _op _chroot apt-mark hold raspberrypi-ui-mods
-_op _chroot apt-mark hold linux-headers-rpi-v8
-_op _chroot apt-mark hold linux-image-rpi-v8
-_op _chroot apt-mark hold linux-headers-rpi-2712
-_op _chroot apt-mark hold linux-image-rpi-2712
+#_op _chroot apt-mark hold linux-headers-rpi-v8
+#_op _chroot apt-mark hold linux-image-rpi-v8
+#_op _chroot apt-mark hold linux-headers-rpi-2712
+#_op _chroot apt-mark hold linux-image-rpi-2712
 
 echo "Installing Updates"
 _apt update || die "Could not update package sources"
@@ -26,7 +26,7 @@ _op _chroot apt-mark unhold libfm-gtk4
 _op _chroot apt-mark unhold libfm-modules
 _op _chroot apt-mark unhold libfm4
 _op _chroot apt-mark unhold raspberrypi-ui-mods
-_op _chroot apt-mark unhold linux-headers-rpi-v8
-_op _chroot apt-mark unhold linux-image-rpi-v8
-_op _chroot apt-mark unhold linux-headers-rpi-2712
-_op _chroot apt-mark unhold linux-image-rpi-2712
+#_op _chroot apt-mark unhold linux-headers-rpi-v8
+#_op _chroot apt-mark unhold linux-image-rpi-v8
+#_op _chroot apt-mark unhold linux-headers-rpi-2712
+#_op _chroot apt-mark unhold linux-image-rpi-2712
