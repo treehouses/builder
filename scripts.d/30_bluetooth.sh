b610bda81e6f17886724ce54ec2ca4c6eec0bfb7
dogi <dogi@users.noreply.github.com>
2025-02-07 22:45:11 -0500

    Update 30_bluetooth.sh

--- scripts.d/30_bluetooth.sh
+++ scripts.d/30_bluetooth.sh
@@ -8,3 +8,5 @@ cat mnt/img_root/usr/local/bin/bluetooth-server.py | grep ^class
 
 echo "Switching bluetooth device class to 0x00010c - computer"
 sed -i -e 's/#Class = .*/Class = 0x00010c/g' mnt/img_root/etc/bluetooth/main.conf
+
+sed -i -e 's#libexec/bluetooth/bluetoothd#sbin/bluetoothd --noplugin=sap#' mnt/img_root/lib/systemd/system/bluetooth.service
