62f1a0c200f35dc7ed1ff4262710482fbfbb3f41
dogi <dogi@users.noreply.github.com>
2025-02-10 17:34:53 -0500

    Update 30_bluetooth.sh

--- scripts.d/30_bluetooth.sh
+++ scripts.d/30_bluetooth.sh
@@ -20,3 +20,24 @@ cat mnt/img_root/lib/systemd/system/bluetooth.service
 mkdir -p mnt/img_root/etc/systemd/system/bluetooth.service.d
 echo -e "[Service]\nExecStart=\nExecStart=/usr/sbin/bluetoothd --noplugin=input --compat" > mnt/img_root/etc/systemd/system/bluetooth.service.d/override.conf
 cat mnt/img_root/etc/systemd/system/bluetooth.service.d/override.conf
+
+cat << EOF > mnt/img_root/etc/systemd/system/rpibluetooth.service
+[Unit]
+Description=Bluetooth Server
+After=bluetooth.service
+Requires=bluetooth.service
+
+StartLimitIntervalSec=500
+StartLimitBurst=5
+
+[Service]
+ExecStart=/usr/bin/python3 /usr/local/bin/bluetooth-server.py
+Restart=always
+RestartSec=5s
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+_op _chroot chmod +x /etc/systemd/system/rpibluetooth.service
+_op _chroot systemctl enable rpibluetooth
