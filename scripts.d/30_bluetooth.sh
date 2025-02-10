0a2610aadfdb7538462574c18588f3eb68d53171
dogi <dogi@users.noreply.github.com>
2025-02-10 14:33:36 -0500

    Update 30_bluetooth.sh

--- scripts.d/30_bluetooth.sh
+++ scripts.d/30_bluetooth.sh
@@ -11,12 +11,12 @@ sed -i -e 's/#Class = .*/Class = 0x00010c/g' mnt/img_root/etc/bluetooth/main.con
 sed -i -e 's/#DiscoverableTimeout = 0/DiscoverableTimeout = 0/g' mnt/img_root/etc/bluetooth/main.conf
 sed -i -e 's/#JustWorksRepairing = never/JustWorksRepairing = always/g' mnt/img_root/etc/bluetooth/main.conf
 sed -i -e 's/#FastConnectable = false/FastConnectable = true/g' mnt/img_root/etc/bluetooth/main.conf
-echo "\n[Agent]\nDefaultAgent = true\nEnableSecurity = false" >> mnt/img_root/etc/bluetooth/main.conf
+echo -e "\n[Agent]\nDefaultAgent = true\nEnableSecurity = false" >> mnt/img_root/etc/bluetooth/main.conf
 cat mnt/img_root/etc/bluetooth/main.conf
 
 sed -i -e 's#libexec/bluetooth/bluetoothd#sbin/bluetoothd#' mnt/img_root/lib/systemd/system/bluetooth.service
 cat mnt/img_root/lib/systemd/system/bluetooth.service
 
 mkdir -p mnt/img_root/etc/systemd/system/bluetooth.service.d
-echo "[Service]\nExecStart=\nExecStart=/usr/sbin/bluetoothd --compat" > mnt/img_root/etc/systemd/system/bluetooth.service.d/override.conf
+echo -e "[Service]\nExecStart=\nExecStart=/usr/sbin/bluetoothd --compat" > mnt/img_root/etc/systemd/system/bluetooth.service.d/override.conf
 cat mnt/img_root/etc/systemd/system/bluetooth.service.d/override.conf
