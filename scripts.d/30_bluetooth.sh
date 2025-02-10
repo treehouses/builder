447db2e3852b6f7e8dc72ff590c337d815b94ca3
dogi <dogi@users.noreply.github.com>
2025-02-10 13:01:13 -0500

    Update 30_bluetooth.sh

--- scripts.d/30_bluetooth.sh
+++ scripts.d/30_bluetooth.sh
@@ -8,7 +8,14 @@ cat mnt/img_root/usr/local/bin/bluetooth-server.py | grep ^class
 
 echo "Switching bluetooth device class to 0x00010c - computer"
 sed -i -e 's/#Class = .*/Class = 0x00010c/g' mnt/img_root/etc/bluetooth/main.conf
+sed -i -e 's/#DiscoverableTimeout = 0/DiscoverableTimeout = 0/g' mnt/img_root/etc/bluetooth/main.conf
+sed -i -e 's/#JustWorksRepairing = never/JustWorksRepairing = always/g' mnt/img_root/etc/bluetooth/main.conf
+sed -i -e 's/#FastConnectable = false/FastConnectable = true/g' mnt/img_root/etc/bluetooth/main.conf
+echo "\n[Agent]\nDefaultAgent = true\nEnableSecurity = false" >> mnt/img_root/etc/bluetooth/main.conf
 cat mnt/img_root/etc/bluetooth/main.conf
 
-sed -i -e 's#libexec/bluetooth/bluetoothd#sbin/bluetoothd --noplugin=sap#' mnt/img_root/lib/systemd/system/bluetooth.service
+sed -i -e 's#libexec/bluetooth/bluetoothd#sbin/bluetoothd#' mnt/img_root/lib/systemd/system/bluetooth.service
 cat mnt/img_root/lib/systemd/system/bluetooth.service
+
+echo "[Service]\nExecStart=\nExecStart=/usr/sbin/bluetoothd --compat" > mnt/img_root/etc/systemd/system/bluetooth.service.d/override.conf
+cat mnt/img_root/etc/systemd/system/bluetooth.service.d/override.conf
