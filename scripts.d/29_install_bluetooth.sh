f84f40759a1a3916dccfbb844a8e1b0ad31fa568
dogi <dogi@users.noreply.github.com>
2025-02-10 17:32:55 -0500

    Update 29_install_bluetooth.sh

--- scripts.d/29_install_bluetooth.sh
+++ scripts.d/29_install_bluetooth.sh
@@ -4,18 +4,17 @@ source lib.sh
 
 cat << EOF > mnt/img_root/etc/systemd/system/rpibluetooth.service
 [Unit]
-Description=Bluetooth server
+Description=Bluetooth Server
 After=bluetooth.service
-Requires=rpibluetooth.service bluetooth.service
+Requires=bluetooth.service
 
 StartLimitIntervalSec=500
 StartLimitBurst=5
 
 [Service]
+ExecStart=/usr/bin/python3 /usr/local/bin/bluetooth-server.py
 Restart=always
 RestartSec=5s
-
-ExecStart=/usr/bin/python3 /usr/local/bin/bluetooth-server.py &
 
 [Install]
 WantedBy=multi-user.target
